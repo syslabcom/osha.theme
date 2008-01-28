11b9ff134f7d952b7135e8a519cccb5b0305cfa6
mishunov <mishunov@97d050b9-b799-4550-a901-4f04598d4601>
2008-01-28 20:53:23 +0000

    implemented two new portlet managers for portlets above and below content

--- osha/theme/browser/interfaces.py
+++ osha/theme/browser/interfaces.py
@@ -1,6 +1,8 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 from zope.viewlet.interfaces import IViewletManager
 
+from plone.portlets.interfaces import IPortletManager
+
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 skin layer.
     """
@@ -12,3 +14,11 @@ class IOSHAHeaderTopactions(IViewletManager):
 class IOSHAHeaderDropdowns(IViewletManager):
     """A viewlet manager with top dropdowns, incl. language selector
     """
+
+class IOSHAAboveContent(IPortletManager):
+    """Portlet manager above the content area.
+    """   
+    
+class IOSHABelowContent(IPortletManager):
+    """Portlet manager below the content area.
+    """     
