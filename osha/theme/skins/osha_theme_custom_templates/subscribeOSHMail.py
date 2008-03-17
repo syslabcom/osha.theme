e21e0d98246f3a68249efaaca223d14bf7139664
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-03-17 12:29:09 +0000

    overwriting some alertservice skins

---
+++ osha/theme/skins/osha_theme_custom_templates/subscribeOSHMail.py
@@ -0,0 +1,61 @@
+## Script (Python) "subscribeOSHMail"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=emailaddress='', noredirect=False
+##title=subscribeOSHMail
+##
+""" helper method to enable osh mail subscription to anonymous user """
+from Products.CMFCore.utils import getToolByName
+from slc.alertservice import AlertMessageFactory as _
+
+ptool = getToolByName(context, 'portal_url')
+portal = ptool.getPortalObject()
+pp = getToolByName(portal, 'portal_properties')
+op = getattr(pp, 'osha_properties', None)
+siteadmin = getattr(portal, 'email_from_address')
+reg_tool = getToolByName(context, 'portal_registration')
+
+REQUEST = context.REQUEST
+if not emailaddress:
+    emailaddress = REQUEST.get('emailaddress', '')
+referer = REQUEST.get('HTTP_REFERER').split('?')[0]+'?'
+qs =REQUEST.get('QUERY_STRING', '')
+if qs:
+    referer += '?'+qs+'&'
+
+if reg_tool.isValidEmail(emailaddress):
+    pass
+else:
+    msg = _(u'You did not enter a valid email address.')
+    try: 
+        msg = unicode(msg, 'iso8859-1').encode('utf-8')
+    except: 
+        pass
+    return REQUEST.RESPONSE.redirect(referer+"portal_status_message="+msg)
+
+
+if REQUEST.get('submit')=='unsubscribe':
+    mesg = "UNSUBSCRIBE OSHMAIL\n"
+    mssg = "Your unsubscription request has been sent."
+else:
+    mesg = "SUBSCRIBE OSHMAIL anonymous\n"
+    mssg = "Your subscription request has been sent."
+
+
+recipient = op.getProperty('listserv_email', siteadmin)
+
+sender = emailaddress
+
+subject = ''
+try:
+    context.MailHost.secureSend(message=mesg , mto=recipient, mfrom=sender, subject=subject)
+except Exception, e:
+    mssg = "Your subscription could not be sent. Please try again."
+
+if not noredirect:
+    from slc.alertservice.utils import encodeEmail
+    feedbackpage = "http://osha.europa.eu/news/oshmail/subscription_feedback?portal_status_message=%s&e=%s" % (mssg, encodeEmail(sender))
+    return REQUEST.RESPONSE.redirect(feedbackpage)
---
+++ osha/theme/skins/osha_theme_custom_templates/subscribe_alert.cpy
@@ -0,0 +1,47 @@
+## Controller Python Script "subscribe_alert"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind state=state
+##bind subpath=traverse_subpath
+##parameters=
+##title=Subscribe to alerts by user email address
+##
+from Products.CMFCore.utils import getToolByName
+request = context.REQUEST
+error = ''
+from slc.alertservice import AlertMessageFactory as _
+
+
+alerttool = getToolByName(context, 'portal_alertservice')
+reg_tool = getToolByName(context, 'portal_registration')
+
+
+# retrieving alert settings
+if request.has_key('form.button.Save'):
+    email = request.get('email', '')
+    if reg_tool.isValidEmail(email):
+        pass
+    else:
+        msg = _(u'You did not enter a valid email address.')
+        state.setError('email', 
+                        msg,
+                       'invalid_email')
+        return state.set(status='emailfailure', context=context, portal_status_message='Please correct the indicated errors.')
+
+# Subscribing to a new alert
+
+error = alerttool.handleSubscription(request)
+
+# Subscribe to oshmail if user has requested this
+if request.get('oshmail', 0):
+    context.subscribeOSHMail(emailaddress=email,noredirect=True)
+
+error = error.strip()
+
+if error=='':
+    return state.set(status='success', context=context, portal_status_message='Subscription successful.')
+else:
+    return state.set(status='failure', context=context, portal_status_message='Please correct the indicated errors. %s' % [error])
+
