2f4f954785607b32d33a44f32bd12ed1e7a529e1
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-05-08 17:23:34 +0000

    improved version with working ram.cache-ing

--- osha/theme/browser/vocabulary.py
+++ osha/theme/browser/vocabulary.py
@@ -1,48 +1,57 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
-#from plone.memoize.forever import memoize
 from plone.memoize import ram
-from time import time
 
 class VocabularyPathView(BrowserView):
     """View for calculating complete paths of hierarchical vocabulary entries
     """
     def __call__(self, fieldName=''):
-        
         field = self.context.getField(fieldName)
         if not field:
             return []
-#        import pdb; pdb.set_trace()
+
         termUID = field.getRaw(self.context)
-        pvt = getToolByName(self.context, 'portal_vocabularies')
-        VOCAB = getattr(pvt, field.widget.vocabulary, None)
-        vd = VOCAB.getVocabularyDict(VOCAB)
-
-        parents_map = dict()
-        # list of current parents
-        self.cp = list()
+        vocabname = field.widget.vocabulary
         
-        def _recurseDict_cachekey(method, vocab_dict, level):
-            return (vocab_dict.keys())
-
-#        @ram.cache(_recurseDict_cachekey)
-#        @ram.cache(lambda *args: time() // (60 * 60))
-        def recurseDict(vocab_dict, level):
-            for k in vocab_dict.keys():
-                if level==0: 
-                    self.cp = list()
-                else:
-                    self.cp = self.cp[:level]
-                    parents_map[k] = [x for x in self.cp]
-                vd = vocab_dict[k][1]
-                if vd:
-                    self.cp.append(k)
-                    recurseDict(vd, level+1)
-        recurseDict(vd, 0)
-
+        parents_map = self.getParentsMap(vocabname)
         res = set()
         for term in termUID:
             res.update(parents_map.get(term, []) + [term])
 
         return list(res)
+
+
+    def _getParentsMap_cachekey(method, self, vocabname):
+        return (vocabname,)
+
+    @ram.cache(_getParentsMap_cachekey)
+    def getParentsMap(self, vocabname):
+        pvt = getToolByName(self.context, 'portal_vocabularies')
+        VOCAB = getattr(pvt, vocabname, None)
+        # get the complete dictionary
+        vd = VOCAB.getVocabularyDict(VOCAB)
+
+        # the mapping holds a list of parent ids for every term id
+        parents_map = dict()
+        # list of current parents
+        self.cp = list()
+
+        def recurseDict(vocab_dict, level):
+            for k in vocab_dict.keys():
+                # clear the list of current parents for top level nodes
+                if level==0:
+                    self.cp = list()
+                else:
+                    # if it is not a root node, prune the list of current parents
+                    #  ( it cannot be longer than the current level )
+                    self.cp = self.cp[:level]
+                    # and add the parents to the mapping
+                    parents_map[k] = [x for x in self.cp]
+                vd = vocab_dict[k][1]
+                if vd:
+                    # recurse one level deeper
+                    self.cp.append(k)
+                    recurseDict(vd, level+1)
+        recurseDict(vd, 0)
+        return parents_map
