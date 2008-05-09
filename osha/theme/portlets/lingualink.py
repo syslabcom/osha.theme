9a0da63aaa42ac1d4f26420316dda0d6a4a576aa
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-05-09 09:50:27 +0000

    added lingualink portlet

---
+++ osha/theme/portlets/lingualink.py
@@ -0,0 +1,59 @@
+import Acquisition
+from zope.component import getMultiAdapter
+from zope.formlib import form
+from zope.interface import implements
+
+from plone.app.portlets.portlets import base
+from plone.memoize.instance import memoize
+from plone.portlets.interfaces import IPortletDataProvider
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFPlone import PloneMessageFactory as _
+from Products.CMFCore.utils import getToolByName
+
+from Products.BlueLinguaLink.browser import LinguaLinkPortlet
+
+class ILinguaLinkPortlet(IPortletDataProvider):    
+    pass
+
+class Assignment(base.Assignment):
+    implements(ILinguaLinkPortlet)
+
+    @property
+    def title(self):
+        return _(u"LinguaLink Portlet")
+
+class Renderer(base.Renderer, LinguaLinkPortlet):
+
+    render = ViewPageTemplateFile('lingualink.pt')
+
+    def __init__(self, *args):
+        base.Renderer.__init__(self, *args)
+        portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+        self.portal = portal_state.portal()
+                
+    @property
+    def available(self):
+        return self._data()
+        
+    def title(self):
+        return _(u"LinguaLinks")
+        
+        
+    def check_access(self):
+        portal_membership = getToolByName(self.context, 'portal_membership')
+        member = portal_membership.getAuthenticatedMember()
+        return member and member.hasRole('Manager')        
+        
+    @memoize
+    def _data(self):
+        return True
+
+
+class AddForm(base.NullAddForm):
+    form_fields = form.Fields(ILinguaLinkPortlet)
+    label = _(u"Add LinguaLink Portlet")
+    description = _(u"Manage LinguaLinks")
+
+    def create(self):
+        return Assignment()
