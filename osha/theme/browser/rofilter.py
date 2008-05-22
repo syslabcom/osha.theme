cf1c6a71dd7a04b5e70abb5de672ef1dfd5da45b
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-05-22 05:42:42 +0000

    added filter for riskob

---
+++ osha/theme/browser/rofilter.py
@@ -0,0 +1,56 @@
+import Acquisition
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+
+class ROFilterView(BrowserView):
+    """View for displaying the ro content filter page 
+    """
+    template = ViewPageTemplateFile('templates/rofilter.pt')
+    template.id = "ro_filter"
+
+    def __call__(self):
+        self.request.set('disable_border', True)
+
+        return self.template() 
+
+    def search_portal_types(self):
+        """ compute the list of query params to search for portal_types"""
+        context = Acquisition.aq_inner(self.context)    
+        query = {}       
+        query.update({'portal_type': 'RichDocument'})
+        return query
+        
+    def buildQuery(self):
+        """ Build the query based on the request """
+        context = Acquisition.aq_inner(self.context)
+        query = { 'sort_on': 'effective',
+                  'sort_order':'reverse',
+                  'Language': ''}
+
+                  
+
+        ero_topic = self.request.get('ero_topic', None)
+        if ero_topic:
+            query.update({'ero_topic':ero_topic})
+
+        ero_target_group = self.request.get('ero_target_group', None)
+        if ero_target_group:
+            query.update({'ero_target_group':ero_target_group})
+
+        country = self.request.get('country', '')
+        if country:
+            query.update({'country':country})
+
+        SearchableText = self.request.get('SearchableText', '')
+        if SearchableText != '':
+            query.update({'SearchableText':SearchableText})
+
+        search_portal_types = self.search_portal_types()
+        query.update(search_portal_types)
+
+        return query
+        
+        
+
+        
