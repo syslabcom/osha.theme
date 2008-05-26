3b65af03c3eb915650d70afce21c7872f4ee400d
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-05-26 11:49:43 +0000

    added view "resource_listing" to display all files in the current directory (eg for campaign resources)

---
+++ osha/theme/browser/resource_listing.py
@@ -0,0 +1,30 @@
+from Acquisition import aq_base
+#from plone.memoize import ram
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+
+class ResourceListingView(BrowserView):
+    """View for displaying resources such as campaign materials
+    """
+    template = ViewPageTemplateFile('templates/resource_listing.pt')
+    template.id = "resource_listing"
+    
+    def __call__(self):
+        self.request.set('disable_border', True)
+
+        self.items = self._getItems()
+        self.description = self.context.Description()
+        if getattr(aq_base(self.context), 'content', None):
+            content = getattr(self.context, 'content')
+            if hasattr(content, 'getText') and callable(content.getText):
+                self.description = content.getText()
+        return self.template()
+
+    def _getItems(self):
+        pwt = getToolByName(self.context, 'portal_workflow')
+        objects = [x for x in self.context.objectValues(['ATFile', 'ATImage', 'ATBlob'])
+                if pwt.getInfoFor(x, 'review_state') == 'published' ]
+        
+        items = [dict(title=x.title_or_id(), description=x.Description(), link=x.absolute_url()) for x in objects]
+        return items
