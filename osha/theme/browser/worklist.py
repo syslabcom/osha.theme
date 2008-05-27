aa85c3a46df49e753a15914e274d9c3c6e350f84
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-05-27 08:49:55 +0000

    author points to "Any" per default again; all content types arer initially selected

--- osha/theme/browser/worklist.py
+++ osha/theme/browser/worklist.py
@@ -16,6 +16,25 @@ class WorklistView(DBFilterView):
 
         return self.template() 
 
+    def search_types(self):
+        """ returns a list of translated search types to select from 
+        This method is overwritten from dbfilter to provide a default list of types, so that all 
+        are selected initially"""
+        context = Acquisition.aq_inner(self.context)
+        
+        default = ['OSH_Link', 'RALink', 'CaseStudy', 'Provider', 'Publication']
+        local_portal_types = context.getProperty('search_portal_types', default)
+        search_portal_types = self.request.get('search_portal_types', local_portal_types)
+        
+        TYPES = [ 
+            ('OSH Link', 'OSH_Link', 'OSH_Link' in search_portal_types) ,
+            ('Risk Assessment Link', 'RALink', 'RALink' in search_portal_types) ,
+            ('Case Study', 'CaseStudy', 'CaseStudy' in search_portal_types) ,
+            ('Provider', 'Provider', 'Provider' in search_portal_types) ,
+            ('Publication', 'Publication', 'Publication' in search_portal_types)
+                ]
+        return TYPES
+
     def buildQuery(self):
         """ Build the query based on the request """
         context = Acquisition.aq_inner(self.context)
