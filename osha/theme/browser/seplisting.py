0056b44c04e6d84140eb094c151c25b36374fe30
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-05-28 15:49:19 +0000

    added seplisting

---
+++ osha/theme/browser/seplisting.py
@@ -0,0 +1,20 @@
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+
+class SEPListingView(BrowserView):
+    """View for displaying the dynamic SEP overview page at /topics, /sector, /priority_groups
+    """
+    template = ViewPageTemplateFile('templates/seplisting.pt')
+    
+    def __call__(self):
+        return self.template() 
+
+    def getAreaLinks(self):
+        """ return the SEPS under topics """
+        path = "/".join(self.context.getPhysicalPath())
+        pc = getToolByName(self.context, 'portal_catalog')
+        res = pc(path={'query': path, 'depth': 1}, portal_type="Folder", review_state='published', sort_on='sortable_title')
+        return res
+        
+        
