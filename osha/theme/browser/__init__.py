fa974f28ac42544e00ba6531e1bdd53a9c9b685a
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-06-01 10:07:55 +0000

    OSHAMessageFactory is now defined in OSHATranslations

--- osha/theme/browser/__init__.py
+++ osha/theme/browser/__init__.py
@@ -1,3 +1,4 @@
 #
-from zope.i18nmessageid import MessageFactory
-OSHAMessageFactory = MessageFactory('osha')
+#Note: OSHAMessageFactory is now defined in OSHATranslations
+#from zope.i18nmessageid import MessageFactory
+#OSHAMessageFactory = MessageFactory('osha')
