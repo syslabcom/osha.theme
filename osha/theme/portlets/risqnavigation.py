b96f42c7785f51feec97b8dd0a35df07e88996d7
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-06-03 17:48:27 +0000

    added dyno navi

--- osha/theme/portlets/risqnavigation.py
+++ osha/theme/portlets/risqnavigation.py
@@ -1,4 +1,5 @@
 from plone.app.portlets.portlets import navigation
+import Acquisition
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -7,3 +8,27 @@ class Renderer(navigation.Renderer):
     
     _template = ViewPageTemplateFile('risqnavigation.pt')
     recurse = ViewPageTemplateFile('risqnavigation_recurse.pt')
+    
+    linklist = ['index_html', 'why_risq_it', 'what_can_you_risq', '../competition/video', 'edge_safety']
+    
+    def links(self):
+        context = Acquisition.aq_inner(self.context)      
+        mylist = []    
+        P = None
+        currpath = self.request.VIRTUAL_URL_PARTS[1]
+        
+        for P in self.request.PARENTS:
+            if P.getId()== "risq":
+                break
+        cnt = 0
+        for l in linklist:
+            cnt += 1
+            ob = P.restrictedTraverse(l, None)
+            state = False
+            if ob is not None:
+                obpath = "/".join(ob.getPhysicalPath())
+                if currpath in obpath:
+                    state = True
+                mylist.append((ob, state, cnt))
+            
+        return mylist
