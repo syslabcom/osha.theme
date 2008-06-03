f5f2106d1bd263b3515997902f13983b0b6daa8d
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-06-03 11:10:11 +0000

    overwrote collection renderer to use oshtopic_view for more link if collection is out of context

---
+++ osha/theme/portlets/collection.py
@@ -0,0 +1,36 @@
+import Acquisition
+from plone.portlet.collection import collection
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+    
+
+class Renderer(collection.Renderer):
+    """Portlet renderer.
+    
+    This is registered in configure.zcml. The referenced page template is
+    rendered, and the implicit variable 'view' will refer to an instance
+    of this class. Other methods can be added and referenced in the template.
+    """    
+    _template = ViewPageTemplateFile('collection.pt')
+
+
+    def collection_url(self):
+        collection = self.collection()
+        if collection is None:
+            return None
+
+
+        collectionpath = "/".join(collection.getPhysicalPath())
+        
+        context = Acquisition.aq_inner(self.context)
+        if not context.isPrincipiaFolderish:
+            context = Acquisition.aq_parent(context)
+        contextpath = "/".join(context.getPhysicalPath())
+        
+        # If the shown collection is within the current context, reference it directly
+        if collectionpath.startswith(contextpath):
+            return collection.absolute_url()
+        # If the collection is in a different place in the site, use the proxy view
+        else:
+            return "%s/@@oshtopic-view?tp=%s" % (context.absolute_url(), collectionpath)
+            
+            
