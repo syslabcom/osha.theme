18828cdddfc610a8bda1b6557ac68c58e1fcbec2
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-06-10 10:30:05 +0000

    more intelligent caching, that considers the url of the used collection

--- osha/theme/portlets/collection.py
+++ osha/theme/portlets/collection.py
@@ -3,10 +3,37 @@ from plone.portlet.collection import collection
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from time import time
-from plone.app.portlets.cache import render_cachekey
+from plone.app.portlets.cache import get_language
 from plone.memoize import ram
 from plone.memoize.compress import xhtml_compress
 from plone.memoize.instance import memoize
+
+def render_cachekey(fun, self):
+    """
+    Generates a key based on:
+
+    * Portal URL
+    * Negotiated language
+    * Anonymous user flag
+    * Portlet manager
+    * Assignment
+    * URL of collection used (instead of using _data)
+    
+    """
+    context = Acquisition.aq_inner(self.context)
+
+    fingerprint = self.collection_url()
+    anonymous = getToolByName(context, 'portal_membership').isAnonymousUser()
+
+    key= "".join((
+        getToolByName(Acquisition.aq_inner(self.context), 'portal_url')(),
+        get_language(Acquisition.aq_inner(self.context), self.request),
+        str(anonymous),
+        self.manager.__name__,
+        self.data.__name__,
+        fingerprint))
+    return key
+
 
 class Renderer(collection.Renderer):
     """Portlet renderer.
@@ -17,7 +44,8 @@ class Renderer(collection.Renderer):
     """    
     _template = ViewPageTemplateFile('collection.pt')
 
-    @ram.cache(lambda *args: time() // (60 * 10))
+#    @ram.cache(lambda *args: time() // (60 * 10))
+    @ram.cache(render_cachekey)
     def render(self):
         return xhtml_compress(self._template())
 
