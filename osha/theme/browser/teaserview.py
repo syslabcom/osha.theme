6657c9c685147224d440b9071b40d47b4763bd55
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-06-16 10:26:27 +0000

    added teaser-archive-view

--- osha/theme/browser/teaserview.py
+++ osha/theme/browser/teaserview.py
@@ -51,3 +51,50 @@ class TeaserView(BrowserView):
                        
         self.items = catalog(query)
         return self.template()
+
+
+class TeaserArchiveView(BrowserView):
+    """ used for displaying archived  teasers"""
+    
+    template = ViewPageTemplateFile('templates/teaser_view.pt')
+    template.id = "teaser-view"
+
+    def __call__(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        portal_languages = getToolByName(self.context, 'portal_languages')
+        preflang = portal_languages.getPreferredLanguage()
+
+
+        # search in the navigation root of the currently selected language and in the English path
+        # with Language = preferredLanguage or neutral
+        paths = list()
+        portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+        navigation_root_path = portal_state.navigation_root_path()
+        navigation_root = portal_state.portal().restrictedTraverse(navigation_root_path)
+        teaser = getattr(navigation_root, 'teaser', None)
+        if teaser:
+            teaser_path = '/'.join(teaser.getPhysicalPath())
+            paths.append(teaser_path)
+            try:
+                canonical_path = '/'.join(teaser.getCanonical().getPhysicalPath())
+                paths.append(canonical_path)
+            except:
+                
+                pass
+
+        state = 'published'
+        now = DateTime()
+        query = dict(portal_type='News Item',
+                       review_state=state,
+                       path=paths,
+                       effective={'query': now,
+                            'range': 'max'},
+                       expires={'query': now,
+                            'range': 'max'},
+                       sort_on='effective',
+                       sort_order='reverse',
+                       Language=['', preflang])
+                       
+        self.items = catalog(query)
+        return self.template()
