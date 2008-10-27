86491d6dd463b9dc5b516c8068717b614af5899f
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-10-27 11:03:30 +0000

    1) If teaser-view is called on a Topic, use Topic's query, if present 2) Show contents of getText, if present in PT 3) use min-width instead of width for image-div, to cope with oversize images

--- osha/theme/browser/teaserview.py
+++ osha/theme/browser/teaserview.py
@@ -1,7 +1,7 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
-from Acquisition import aq_inner, aq_parent
+from Acquisition import aq_inner, aq_parent, aq_base
 from zope.component import getMultiAdapter
 from DateTime import DateTime
 
@@ -18,39 +18,48 @@ class TeaserView(BrowserView):
         portal_languages = getToolByName(self.context, 'portal_languages')
         preflang = portal_languages.getPreferredLanguage()
 
+        # try to get query parameters from Topic (if present)
+        query = getattr(context, 'buildQuery', None) and context.buildQuery()
 
-        # search in the navigation root of the currently selected language and in the English path
-        # with Language = preferredLanguage or neutral
-        paths = list()
-        portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
-        navigation_root_path = portal_state.navigation_root_path()
-        navigation_root = portal_state.portal().restrictedTraverse(navigation_root_path)
-        teaser = getattr(navigation_root, 'teaser', None)
-        if teaser:
-            teaser_path = '/'.join(teaser.getPhysicalPath())
-            paths.append(teaser_path)
-            try:
-                canonical_path = '/'.join(teaser.getCanonical().getPhysicalPath())
-                paths.append(canonical_path)
-            except:
-                
-                pass
+        if not query:
+            # search in the navigation root of the currently selected language and in the English path
+            # with Language = preferredLanguage or neutral
+            paths = list()
+            portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+            navigation_root_path = portal_state.navigation_root_path()
+            navigation_root = portal_state.portal().restrictedTraverse(navigation_root_path)
+            teaser = getattr(navigation_root, 'teaser', None)
+            if teaser:
+                teaser_path = '/'.join(teaser.getPhysicalPath())
+                paths.append(teaser_path)
+                try:
+                    canonical_path = '/'.join(teaser.getCanonical().getPhysicalPath())
+                    paths.append(canonical_path)
+                except:
+                    pass
 
-        state = 'published'
-        now = DateTime()
-        query = dict(portal_type='News Item',
-                       review_state=state,
-                       path=paths,
-                       effective={'query': now,
-                            'range': 'max'},
-                       expires={'query': now,
-                            'range': 'min'},
-                       sort_on='effective',
-                       sort_order='reverse',
-                       Language=['', preflang])
-                       
+            state = 'published'
+            now = DateTime()
+            query = dict(portal_type='News Item',
+                           review_state=state,
+                           path=paths,
+                           effective={'query': now,
+                                'range': 'max'},
+                           expires={'query': now,
+                                'range': 'min'},
+                           sort_on='effective',
+                           sort_order='reverse',
+                           Language=['', preflang])
+
         self.items = catalog(query)
         return self.template()
+
+
+    def getBodyText(self):
+        """ returns body text of collection  if present """
+        context = aq_base(aq_inner(self.context))
+        text = getattr(context, 'getText', None) and context.getText() or ''
+        return text
 
 
 class TeaserArchiveView(BrowserView):
