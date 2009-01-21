ca9bb2e3d3ff0854f638ef63a98244c0649d12d9
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-01-21 17:54:08 +0000

    added special navigation portlet for the Safestart theme

---
+++ osha/theme/portlets/safestartnavigation.py
@@ -0,0 +1,21 @@
+from plone.app.portlets.portlets import navigation
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Acquisition import aq_base, aq_inner, aq_parent
+from Products.CMFPlone import utils
+
+class Renderer(navigation.Renderer):
+    """Dynamically override standard header for navtree portlet"""
+    
+    _template = ViewPageTemplateFile('safestartnavigation.pt')
+    recurse = ViewPageTemplateFile('safestartnavigation_recurse.pt')
+    
+
+    def root_item_class(self):
+        context = aq_inner(self.context)
+        root = self.getNavRoot()
+        if (aq_base(root) is aq_base(context) or
+                (aq_base(root) is aq_base(aq_parent(aq_inner(context))) and
+                utils.isDefaultPage(context, self.request, context))):
+            return 'active'
+        else:
+            return 'default'
