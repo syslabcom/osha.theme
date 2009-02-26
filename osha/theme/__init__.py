700c1211e4f31fbdbbe2f1c6d37d09436c0f1b77
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-02-26 16:29:07 +0000

    Set module security for config

--- osha/theme/__init__.py
+++ osha/theme/__init__.py
@@ -1,5 +1,9 @@
 from zope.i18nmessageid import MessageFactory
+
+from AccessControl import ModuleSecurityInfo
+
 OSHAMessageFactory = MessageFactory('osha')
 
 def initialize(context):
     """Intializer called when used as a Zope 2 product."""
+    ModuleSecurityInfo('osha.theme.config').declarePublic('EUROPEAN_NETWORK')
