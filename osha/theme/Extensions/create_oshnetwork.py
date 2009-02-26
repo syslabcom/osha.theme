942c6fd0703d4aa761cee91cb80d4105e85001c5
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-02-26 16:29:58 +0000

    Add additional methods to set up FOP section.

--- osha/theme/Extensions/create_oshnetwork.py
+++ osha/theme/Extensions/create_oshnetwork.py
@@ -1,21 +1,82 @@
+from zope.app.container.interfaces import INameChooser
+
+from zope.component import getUtility, getMultiAdapter
+
+from plone.app.contentrules.rule import Rule
+from plone.app.portlets.interfaces import ILeftColumn, IRightColumn
+from plone.app.portlets.storage import PortletAssignmentMapping 
+from plone.app.portlets.portlets import classic
+
+from plone.contentrules.engine.interfaces import IRuleStorage
+from plone.contentrules.rule.interfaces import IRuleAction
+
+from plone.portlets.interfaces import IPortletType 
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignmentMapping
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from plone.portlets.constants import CONTEXT_CATEGORY as CONTEXT_PORTLETS
+
+from AccessControl import getSecurityManager
+from AccessControl.SecurityManagement import newSecurityManager
+from StringIO import StringIO
+
 from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.permissions import ManagePortal
+from Products.SEPStructure.util import addContentRule, assignRuleToObject
 
 from osha.theme.config import EUROPEAN_NETWORK
 
-def custom_view(self):
-    return self.oshnetwork_country_view()
-
 def run(self):
-    """ - Create folder called oshnetwork (inside 'en' folder).
-        - Add a subfolder per country 
+    """ - Create folder called oshnetwork (inside 'en' folder). DONE
+        - Add a subfolder per country  DONE
+        - Add an empty index page. DONE
+        - Implement the country dropdown (classic portlet) DONE
+        - Block the portlets DONE
+        - Content rule (on publish) for news and events added to the FOP 
         - In the navigation portlet, move it below about.
-        - Add and empty index page.
-        - Block the portlets
-        - Implement the country dropdown (classic portlet)
         - Reuse the document_view to created oshnetwork_view
-        - Content rule (on publish) for news and events added to the FOP
           folder.
     """
+    createContentRules(self) 
+    createOSHNetworkFolder(self)
+    createCountrySubfolders(self)
+    createClassicPortletWithCountryDropdown(self)
+    blockPortlets(self)
+    return 'Finished!'
+
+def createContentRules(self):
+    """ """
+    portal = getToolByName(self, 'portal_url').getPortalObject()
+    rule_id = 'move-news-after-publish'
+    addContentRule(
+        context     = portal, 
+        rule_id     = rule_id, 
+        title       = u'Move published news items to /news folder', 
+        description = u'This rule moves all objects of type NewsItem to the news folder in the site root.', 
+        eventID     = u'Workflow state changed', 
+        conditions  = (
+            ('plone.conditions.PortalType', {'check_types' : ['News Item']}),
+            ('plone.conditions.WorkflowState', {'wf_states' : ['published']}),
+            ), 
+        actions     = (('plone.actions.Move', {'target_folder' : '/news'}),)
+        )
+                      
+    rule_id = 'move-events-after-publish'
+    addContentRule(
+        context     = portal, 
+        rule_id     = rule_id, 
+        title       = u'Move published event items to /events folder', 
+        description = u'This rule moves all objects of type ATEvent to the events folder in the site root.', 
+        eventID     = u'Workflow state changed', 
+        conditions  = (
+            ('plone.conditions.PortalType', {'check_types' : ['Event']}),
+            ('plone.conditions.WorkflowState', {'wf_states' : ['published']}),
+            ),
+        actions     = (('plone.actions.Move', {'target_folder' : '/events'}),),
+        )
+
+def createOSHNetworkFolder(self):
+    """ """
     portal = getToolByName(self, 'portal_url').getPortalObject()
 
     if hasattr(portal, 'en'):
@@ -35,13 +96,21 @@ def run(self):
                             title=title,
                             description=desc)
 
-
     oshnetwork = getattr(parent, 'oshnetwork')
     wftool = getToolByName(self, 'portal_workflow')
     wftool.doActionFor(oshnetwork, 'publish')
 
+    oshnetwork.invokeFactory('Document', 'index.html')
+    document = getattr(oshnetwork, 'index.html')
+    document.setTitle('OSHNetwork')
+    wftool.doActionFor(document, 'publish')
 
-    # Create subfolder with index.hml in each country folder
+def createCountrySubfolders(self):
+    """ Create subfolder with index.hml in each country folder
+    """
+    wftool = getToolByName(self, 'portal_workflow')
+    portal = getToolByName(self, 'portal_url').getPortalObject()
+    oshnetwork = getattr(portal, 'oshnetwork')
     for cc_and_name, link in EUROPEAN_NETWORK:
         try:
             cc, name = cc_and_name.split(' ', 1)
@@ -54,22 +123,40 @@ def run(self):
             continue
         
         if not hasattr(oshnetwork, name.lower()):
-            oshnetwork.invokeFactory('Folder', name.lower(), title=name)
-            folder = getattr(oshnetwork, name.lower())
-            wftool.doActionFor(folder, 'publish')
-            folder.invokeFactory('Document', 'index.html')
-            document = getattr(folder, 'index.html')
-            wftool.doActionFor(document, 'publish')
-    
-    return 'Sucessfully created'
+            oshnetwork.invokeFactory('Folder', cc, title=name)
 
+        folder = getattr(oshnetwork, cc)
+        wftool.doActionFor(folder, 'publish')
 
+        for rule_id in ['move-news-after-publish', 
+                        'move-events-after-publish']:
+            assignRuleToObject(context=folder, 
+                               rule_id = rule_id, 
+                               bubbles=True)
 
+        folder.invokeFactory('Document', 'index.html')
+        document = getattr(folder, 'index.html')
+        document.setTitle(name)
+        wftool.doActionFor(document, 'publish')
 
+def createClassicPortletWithCountryDropdown(self):
+    """ """
+    portal = getToolByName(self, 'portal_url').getPortalObject()
+    obj = getattr(portal, 'oshnetwork')
+    column = getUtility(IPortletManager, name='plone.leftcolumn')
+    manager = getMultiAdapter((obj, column,), IPortletAssignmentMapping)
+    assignment = classic.Assignment(template='oshnetwork_country_select',
+                                    macro='portlet')
+    chooser = INameChooser(manager)
+    manager[chooser.chooseName(None, assignment)] = assignment
 
-            
-
-    
-
-
+def blockPortlets(self):
+    """ Block the showing of portlets in oshnetwork
+    """
+    portal = getToolByName(self, 'portal_url').getPortalObject()
+    obj = getattr(portal, 'oshnetwork')
+    for cname in ['plone.rightcolumn', 'plone.leftcolumn']:
+        column = getUtility(IPortletManager, name=cname)
+        ptass = getMultiAdapter((obj, column,), ILocalPortletAssignmentManager)
+        ptass.setBlacklistStatus(CONTEXT_PORTLETS, True)
 
