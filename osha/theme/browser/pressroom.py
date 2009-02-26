cecf78435ee870979d44e7e558673a5517b423a4
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-02-26 21:26:29 +0000

    added dynamic template small css fix

--- osha/theme/browser/pressroom.py
+++ osha/theme/browser/pressroom.py
@@ -32,3 +32,28 @@ class PressRoomView(BrowserView):
         map = "meltwater"
         rows = sin.sin(map, max_size=2)
         return rows
+        
+        
+class DynamicPressRoomView(BrowserView):
+    implements(IPressRoomView)
+    template = ViewPageTemplateFile('templates/pressroom_dynamic.pt')
+
+    def __call__(self):
+        #self.request.set('disable_border', True)
+        return self.template()
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.result = []
+    
+    def _render_cachekey(method, self):
+        return ('meltwater')
+    
+    #@ram.cache(_render_cachekey)
+    def getFeed(self):
+        context = Acquisition.aq_inner(self.context)
+        sin = getToolByName(context, 'sin_tool')
+        map = "meltwater"
+        rows = sin.sin(map, max_size=2)        
+        return rows        
