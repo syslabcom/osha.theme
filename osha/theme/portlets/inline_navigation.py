a27e4c475e29734c0186ba0688fb2b2b0d077f8c
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-03-19 23:21:12 +0000

    reverted test

--- osha/theme/portlets/inline_navigation.py
+++ osha/theme/portlets/inline_navigation.py
@@ -8,7 +8,6 @@ from Products.CMFPlone import PloneMessageFactory as _
 from zope import schema
 from zope.formlib import form
 from plone.portlets.interfaces import IPortletDataProvider
-from plone.app.portlets.portlets import base
 
 class IInlineNavigationPortlet(IPortletDataProvider):
     """A portlet which can render the navigation tree
@@ -22,61 +21,7 @@ class IInlineNavigationPortlet(IPortletDataProvider):
             default=u"",
             required=False)
     
-#    root = schema.Choice(
-#            title=_(u"label_navigation_root_path", default=u"Root node"),
-#            description=_(u'help_navigation_root',
-#                          default=u"You may search for and choose a folder "
-#                                    "to act as the root of the navigation tree. "
-#                                    "Leave blank to use the Plone site root."),
-#            required=False,
-#            source=SearchableTextSourceBinder({'is_folderish' : True},
-#                                              default_query='path:'))
-                            
-#    includeTop = schema.Bool(
-#            title=_(u"label_include_top_node", default=u"Include top node"),
-#            description=_(u"help_include_top_node",
-#                          default=u"Whether or not to show the top, or 'root', "
-#                                   "node in the navigation tree. This is affected " 
-#                                   "by the 'Start level' setting."),
-#            default=False,
-#            required=False)
-            
-#    currentFolderOnly = schema.Bool(
-#            title=_(u"label_current_folder_only",
-#                    default=u"Only show the contents of the current folder."),
-#            description=_(u"help_current_folder_only",
-#                          default=u"If selected, the navigation tree will "
-#                                   "only show the current folder and its "
-#                                   "children at all times."),
-#            default=True,
-#            required=False)
-    
-#    topLevel = schema.Int(
-#            title=_(u"label_navigation_startlevel", default=u"Start level"),
-#            description=_(u"help_navigation_start_level",
-#                default=u"An integer value that specifies the number of folder "
-#                         "levels below the site root that must be exceeded "
-#                         "before the navigation tree will display. 0 means "
-#                         "that the navigation tree should be displayed "
-#                         "everywhere including pages in the root of the site. "
-#                         "1 means the tree only shows up inside folders "
-#                         "located in the root and downwards, never showing "
-#                         "at the top level."),
-#            default=1,
-#            required=False)
-#    
-#    bottomLevel = schema.Int(
-#            title=_(u"label_navigation_tree_depth",
-#                    default=u"Navigation tree depth"),
-#            description=_(u"help_navigation_tree_depth",
-#                          default=u"How many folders should be included "
-#                                   "before the navigation tree stops. 0 "
-#                                   "means no limit. 1 only includes the "
-#                                   "root folder."),
-#            default=0,
-#            required=False)
-            
-class Assignment(base.Assignment):
+class Assignment(navigation.Assignment):
     implements(IInlineNavigationPortlet)
 
     title = _(u'Inline Navigation')
@@ -102,11 +47,10 @@ class Renderer(navigation.Renderer):
     """
     _template = ViewPageTemplateFile('inline_navigation.pt')
 
-    def render(self):
-        return self._template()
+
 
     
-class AddForm(base.AddForm):
+class AddForm(navigation.AddForm):
     form_fields = form.Fields(IInlineNavigationPortlet)
     #form_fields['root'].custom_widget = UberSelectionWidget
     label = _(u"Add Inline Navigation Portlet")
@@ -120,7 +64,7 @@ class AddForm(base.AddForm):
                           topLevel=data.get('topLevel', 0),
                           bottomLevel=data.get('bottomLevel', 0))
 
-class EditForm(base.EditForm):
+class EditForm(navigation.EditForm):
     form_fields = form.Fields(IInlineNavigationPortlet)
     #form_fields['root'].custom_widget = UberSelectionWidget
     label = _(u"Edit Inline Navigation Portlet")
