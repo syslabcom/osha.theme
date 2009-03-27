cc9909a3ffa05ba0b32cddf927cd626a6b9aee37
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-03-27 16:36:27 +0000

    Add DynamicPressRoomConfiguartionForm.
    Add Accessors to PressRoomView

--- osha/theme/browser/pressroom.py
+++ osha/theme/browser/pressroom.py
@@ -1,17 +1,37 @@
-import Acquisition
+from zope.annotation.interfaces import IAnnotations
+from zope.app.form import CustomWidgetFactory
+from zope.app.form.browser.textwidgets import TextWidget
+from zope.component import getMultiAdapter, getUtility
+from zope.formlib import form
 from zope.interface import implements
-from osha.theme.browser.interfaces import IPressRoomView
-from Products.CMFCore.utils import getToolByName
-from Products.Five import BrowserView
+from zope.i18nmessageid import MessageFactory
+from zope.schema.fieldproperty import FieldProperty
+
+
+import Acquisition
+from OFS.SimpleItem import SimpleItem
+
 from plone.memoize.instance import memoize
 from plone.memoize import ram
-from zope.component import getMultiAdapter, getUtility
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import pagetemplatefile 
+from Products.Five.formlib import formbase
+from Products.Five import BrowserView
+
+from osha.theme.browser.interfaces import IPressRoomView
+from osha.theme.browser.interfaces import IPressRoomConfiguration
+from osha.theme.config import FEED_KEY
+from osha.theme.config import PRESS_CONTACTS_KEY
+from osha.theme.config import KEYWORDS_KEY
+
+from interfaces import IPressRoomConfiguration
+
+_ = MessageFactory('osha.theme')
 
 class PressRoomView(BrowserView):
     implements(IPressRoomView)
-    template = ViewPageTemplateFile('templates/pressroom.pt')
+    template = pagetemplatefile.ViewPageTemplateFile('templates/pressroom.pt')
 
     def __call__(self):
         #self.request.set('disable_border', True)
@@ -36,7 +56,7 @@ class PressRoomView(BrowserView):
         
 class DynamicPressRoomView(BrowserView):
     implements(IPressRoomView)
-    template = ViewPageTemplateFile('templates/pressroom_dynamic.pt')
+    template = pagetemplatefile.ViewPageTemplateFile('templates/pressroom_dynamic.pt')
 
     def __call__(self):
         #self.request.set('disable_border', True)
@@ -52,8 +72,52 @@ class DynamicPressRoomView(BrowserView):
     
     #@ram.cache(_render_cachekey)
     def getFeed(self):
+        context = Acquisition.aq_inner(self.context).getCanonical()
+        annotations = IAnnotations(context)
+        if not annotations.get(FEED_KEY):
+            return []
+        map = annotations[FEED_KEY]
         context = Acquisition.aq_inner(self.context)
         sin = getToolByName(context, 'sin_tool')
-        map = "meltwater"
         rows = sin.sin(map, max_size=2)        
         return rows        
+
+    #@ram.cache(_render_cachekey)
+    def getPressContactFolder(self):
+        context = Acquisition.aq_inner(self.context).getCanonical()
+        annotations = IAnnotations(context)
+        return annotations[PRESS_CONTACTS_KEY]
+
+    #@ram.cache(_render_cachekey)
+    def getKeywords(self):
+        context = Acquisition.aq_inner(self.context).getCanonical()
+        annotations = IAnnotations(context)
+        return annotations[KEYWORDS_KEY]
+
+
+
+class KeywordWidget(TextWidget):
+    """ Override TextWidget to increate the width
+    """
+    displayWidth = 80
+
+kw_widget = CustomWidgetFactory(KeywordWidget)
+
+
+class DynamicPressRoomConfigurationForm(formbase.PageForm):
+    """ """
+    form_fields = form.FormFields(IPressRoomConfiguration)
+    form_fields['keyword_list'].custom_widget = kw_widget
+    label = _(u"Configure this Press Room")
+
+    @form.action("save")
+    def action_save(self, action, data):
+        context = Acquisition.aq_inner(self.context)
+        canonical = context.getCanonical()
+        annotations = IAnnotations(canonical)
+        annotations[FEED_KEY] = data['feed_key']
+        annotations[PRESS_CONTACTS_KEY] = data['press_contacts']
+        annotations[KEYWORDS_KEY] = data['keyword_list']
+        return getattr(context, context.default_view)()
+
+
