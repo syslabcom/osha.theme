201e7d4e041099ca7198fd61001e4361230e8f8e
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-04-29 14:19:12 +0000

    Fix problem of old adjustTitle links not recognised in dialog

--- osha/theme/skins/osha_theme_custom_fckeditor/editor/dialog/fck_link/fck_link.js
+++ osha/theme/skins/osha_theme_custom_fckeditor/editor/dialog/fck_link/fck_link.js
@@ -393,18 +393,17 @@ function LoadSelection()
 	}
     // XXX: Changes by SLC:
     // Look for a WAI-conform new window and prepare the dialog.
-    var wai_selected = oLink.getAttribute( 'wai_blank' ) ;
-    if( wai_selected )
+    // var wai_selected = oLink.getAttribute( 'wai_blank' ) ;
+    var onfocus = oLink.getAttribute('onfocus_fckprotectedatt');
+    if( onfocus )
     {
-        GetE('cmbTarget').value = 'wai_blank';
-        SetTarget( 'wai_blank' );
-        // fill the WAI specials
-        var onfocus = oLink.getAttribute('onfocus_fckprotectedatt');
-        if( onfocus )
-        {
-            onfocus = decodeURIComponent(onfocus) ;
-            var ofv = onfocus.split('="')[1]
-            ofv = ofv.replace(/^"|"$/g,"");
+        onfocus = decodeURIComponent(onfocus);
+        var ofv = onfocus.split('="')[1]
+        ofv = ofv.replace(/^"|"$/g,"");
+        if (ofv == 'adjustTitle(this)') {
+            // fill the WAI specials
+            GetE('cmbTarget').value = 'wai_blank';
+            SetTarget( 'wai_blank' );
 
             // sometimes the IE tries to evalute the attribute value as
             // function and the attribute values is like 'function undefined{......}'
@@ -780,7 +779,6 @@ function Ok()
             // XXX Changes by SLC:
             // 1. Open new window with 'onclick'-handler
             // 2. Define a script which adjusts the title-attribute of the link
-            SetAttribute( oLink, 'wai_blank', 'true' ) ;
             SetAttribute( oLink, 'onclick', 'window.open(this.href); return false;' );
             var eventHandler = GetE('txtAdjustWAITitle').value;
             if (eventHandler != '') {
