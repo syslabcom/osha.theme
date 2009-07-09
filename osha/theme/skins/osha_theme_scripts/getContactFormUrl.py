10b904d07c68efbd3b5c2e0a82c170749ff1dac6
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-07-09 19:54:47 +0000

    made contact-info (for subsites) more sophisticated: 1) show for all subsites, regardless of URL / domain. 2) possibility to add contact_intro into language root - text will be shown as intro above the form 3) If "Support email address" from the oshaprops of the subsite is set, it will be used instead of the global email_from_address

--- osha/theme/skins/osha_theme_scripts/getContactFormUrl.py
+++ osha/theme/skins/osha_theme_scripts/getContactFormUrl.py
@@ -6,11 +6,16 @@
 ##bind subpath=traverse_subpath
 ##title=Return the url of the current subsite
 ##parameters=
+
 pu = context.portal_url
 purl = pu()
-suburl = context.restrictedTraverse('@@oshaview').subsiteRootUrl()
+oshaview = context.restrictedTraverse('@@oshaview')
+suburl = oshaview.subsiteRootUrl()
+sub_path = oshaview.subsiteRootPath()
+site = pu.restrictedTraverse(sub_path)
+
 lang = context.portal_languages.getPreferredLanguage()
-if purl == suburl:
+if not oshaview.isSubsite(site):
     template = 'contact_us'
 else:
     template = 'contact-info'
