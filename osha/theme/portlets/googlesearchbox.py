57dd27069ee250b2c9e961bb46afbebc2d3b5e83
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-07-21 22:31:06 +0000

    re-added atoz index link to google search portlet #344

--- osha/theme/portlets/googlesearchbox.py
+++ osha/theme/portlets/googlesearchbox.py
@@ -1,7 +1,27 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from slc.googlesearch.portlets.searchbox import Renderer as BaseRenderer
+from zope.component import getMultiAdapter
+from Products.CMFCore.utils import getToolByName
+from Acquisition import aq_base, aq_inner
 
 
 class Renderer(BaseRenderer):
     """Dynamically override standard header for search portlet"""
     _template = ViewPageTemplateFile('googlesearchbox.pt')
+    
+    
+    
+    def index_alphabetical(self):
+        self.language = getToolByName(self.context, 'portal_languages').getPreferredLanguage()
+        osha_view = getMultiAdapter((self.context, self.request), name=u'oshaview')
+        self.subsite_url = osha_view.subsiteRootUrl()
+        self.subsite_path = osha_view.subsiteRootPath()
+        return '%s/%s/@@index_alphabetical' %(self.subsite_url, self.language)
+
+
+    def showAtozLink(self):
+        osha_view = getMultiAdapter((self.context, self.request), name=u'oshaview')
+        show = osha_view.get_subsite_property('show_atoz_link')
+        if show is None:
+            show = True
+        return show
