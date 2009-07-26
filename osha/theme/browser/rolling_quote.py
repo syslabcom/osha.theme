89e14ea7708f977c8fd4b6aa44b25d29e2dcf698
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-07-26 21:38:12 +0000

    fixed indentation error

--- osha/theme/browser/rolling_quote.py
+++ osha/theme/browser/rolling_quote.py
@@ -5,8 +5,8 @@ from plone.app.portlets import base
 from plone.portlets.interfaces import IPortletDataProvider
 
 class IRollingQuotes(IPortletDataProvider):
-"""at the moment there is nothing to configure"""
-
+    """at the moment there is nothing to configure"""
+    
 class Assignment(base.Assignment):
     def __init__(self):
         """nothing to initialize here"""
