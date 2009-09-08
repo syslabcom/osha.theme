887aacde49e17ee782fc3774420493a7c2f41e3c
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2009-09-08 23:54:34 +0000

    Adding NetworkMemberLinks portlet

---
+++ osha/theme/portlets/network_member_links.py
@@ -0,0 +1,78 @@
+import Acquisition
+from zope.interface import implements
+from zope import schema
+from zope.formlib import form
+from zope.component import getMultiAdapter
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFPlone import PloneMessageFactory as _
+from Products.CMFCore.utils import getToolByName
+
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.memoize.instance import memoize
+from plone.memoize import ram
+from plone.app.portlets.portlets import base
+
+class IOSHNetworkMemberLinksPortlet(IPortletDataProvider):
+    pass
+
+class Assignment(base.Assignment):
+
+    implements(IOSHNetworkMemberLinksPortlet)
+
+    def __init__(self):
+        pass
+
+    @property
+    def title(self):
+        return "Network Member Links"
+
+
+class Renderer(base.Renderer):
+
+    _template = ViewPageTemplateFile('network_member_links.pt')
+    links = [
+             {'url': 'http://test', 'section': 'simple1', 
+              'linktext': 'link text1', 'title': 'title1'},
+             {'url': 'http://test', 'section': 'simple2', 
+              'linktext': 'link text2', 'title': 'title2'},
+             {'url': 'http://test', 'section': 'simple2', 
+              'linktext': 'link text3', 'title': 'title3'},
+             ]
+    link_sections = set([i["section"] for i in links])
+
+    def _render_cachekey(method, self):
+        preflang = getToolByName(self.context, 'portal_languages').getPreferredLanguage()
+        return (preflang)
+        
+    #@ram.cache(_render_cachekey)
+    def render(self):
+        return self._template()
+
+    @memoize
+    def get_links_by_section(self, section):
+        return [i for i in self.links if i["section"] == section]
+
+    def __init__(self, *args):
+        base.Renderer.__init__(self, *args)
+        context = self.context
+
+class AddForm(base.AddForm):
+    """Portlet add form.
+
+    This is registered in configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display. The create() method actually
+    constructs the assignment that is being added.
+    """
+    form_fields = form.Fields(IOSHNetworkMemberLinksPortlet)
+
+    def create(self, data):
+        return Assignment(**data)
+
+class EditForm(base.EditForm):
+    """Portlet edit form.
+
+    This is registered with configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display.
+    """
+    form_fields = form.Fields(IOSHNetworkMemberLinksPortlet)
--- osha/theme/tests/test_oshnetwork_member_view.py
+++ osha/theme/tests/test_oshnetwork_member_view.py
@@ -1,9 +1,19 @@
-from zope.component import getUtility, getMultiAdapter
 import unittest
 import doctest
 
+from zope.component import getUtility, getMultiAdapter
+
+from plone.portlets.interfaces import IPortletType
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import IPortletRenderer
+
+from plone.app.portlets.storage import PortletAssignmentMapping
+
 from osha.theme.browser import oshnetwork_member_view
 from osha.theme.tests.base import OshaThemeTestCase
+from osha.theme.portlets import network_member_links
 
 class TestView(OshaThemeTestCase):
 
@@ -34,8 +44,102 @@ class TestView(OshaThemeTestCase):
         self.assertEquals(localized_path,
                           "/nl/asdf")
 
+class TestPortlet(OshaThemeTestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager', ))
+
+    def test_portlet_type_registered(self):
+        portlet = getUtility(
+            IPortletType,
+            name='osha.NetworkMemberLinks')
+        self.assertEquals(portlet.addview,
+            'osha.NetworkMemberLinks')
+
+    def test_interfaces(self):
+        portlet = network_member_links.Assignment()
+        self.failUnless(IPortletAssignment.providedBy(portlet))
+        self.failUnless(IPortletDataProvider.providedBy(portlet.data))
+
+    def test_invoke_add_view(self):
+        portlet = getUtility(
+            IPortletType,
+            name='osha.NetworkMemberLinks')
+        mapping = self.portal.restrictedTraverse(
+            '++contextportlets++plone.leftcolumn')
+        for m in mapping.keys():
+            del mapping[m]
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+
+        addview.createAndAdd(data={})
+
+        self.assertEquals(len(mapping), 1)
+        self.failUnless(isinstance(mapping.values()[0],
+                                   network_member_links.Assignment))
+
+    def test_invoke_edit_view(self):
+        mapping = PortletAssignmentMapping()
+        request = self.folder.REQUEST
+
+        mapping['foo'] = network_member_links.Assignment()
+        editview = getMultiAdapter((mapping['foo'], request), name='edit')
+        self.failUnless(isinstance(editview, network_member_links.EditForm))
+
+    def test_obtain_renderer(self):
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.rightcolumn',
+                             context=self.portal)
+
+        assignment = network_member_links.Assignment()
+
+        renderer = getMultiAdapter(
+            (context, request, view, manager, assignment), IPortletRenderer)
+        self.failUnless(isinstance(renderer, network_member_links.Renderer))
+
+
+class TestRenderer(OshaThemeTestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager', ))
+
+    def renderer(self, context=None, request=None, view=None, manager=None,
+                 assignment=None):
+        context = context or self.folder
+        request = request or self.folder.REQUEST
+        view = view or self.folder.restrictedTraverse('@@plone')
+        manager = manager or getUtility(
+            IPortletManager, name='plone.rightcolumn', context=self.portal)
+
+        assignment = assignment or network_member_links.Assignment()
+        return getMultiAdapter((context, request, view, manager, assignment),
+                               IPortletRenderer)
+
+    def populateSite(self):
+        """ Populate the test site with some content. """
+
+        self.portal.invokeFactory("Folder", "en")
+        self.portal.en.invokeFactory("Folder", "belgium")
+        self.portal.en.belgium.invokeFactory("Document", "index_html")
+        ltool = self.portal.portal_languages
+        ltool.addSupportedLanguage('nl')
+
+    def test_render(self):
+        self.populateSite()
+
+        # Test the rendered portlet
+        render_portlet = self.renderer(context=self.portal.en.belgium.index_html,
+                          assignment=network_member_links.Assignment())
+        render_portlet = render_portlet.__of__(self.folder)
+        render_portlet.update()
+
+        # TODO: actually test it :)
+
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(TestView))
+    suite.addTest(makeSuite(TestPortlet))
+    suite.addTest(makeSuite(TestRenderer))
     return suite
