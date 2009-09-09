4f5343ebd003678cd987a1d4402925177e080ae4
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-09-09 14:53:18 +0000

    Strange, it works locally too

--- osha/theme/tests/test_oshnetwork_member_view.py
+++ osha/theme/tests/test_oshnetwork_member_view.py
@@ -140,6 +140,6 @@ def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(TestView))
-    #suite.addTest(makeSuite(TestPortlet))
-    #suite.addTest(makeSuite(TestRenderer))
+    suite.addTest(makeSuite(TestPortlet))
+    suite.addTest(makeSuite(TestRenderer))
     return suite
