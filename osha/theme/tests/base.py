f33011440a8e552d23d52f404f9f839c11c483a8
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2009-09-10 20:48:23 +0000

    the test base module was setting up the osha.theme product incorrectly

--- osha/theme/tests/base.py
+++ osha/theme/tests/base.py
@@ -8,11 +8,6 @@ from osha.theme.config import product_globals
 
 # Let Zope know about the two products we require above-and-beyond a basic
 # Plone install (PloneTestCase takes care of these).
-ztc.installProduct('PloneLanguageTool')
-ztc.installProduct('LinguaPlone')
-ztc.installProduct('SimpleAttachment')
-ztc.installProduct('RichDocument')
-
 # Import PloneTestCase - this registers more products with Zope as a side effect
 from Products.PloneTestCase.PloneTestCase import PloneTestCase
 from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
@@ -25,26 +20,21 @@ SiteLayer = layer.PloneSite
 class OshaThemeLayer(SiteLayer):
     @classmethod
     def setUp(cls):
+        ztc.installProduct('PloneLanguageTool')
+        ztc.installProduct('LinguaPlone')
+        ztc.installProduct('SimpleAttachment')
+        ztc.installProduct('RichDocument')
+
         ztc.installProduct('PlacelessTranslationService')
-        setupPloneSite(products=['SimpleAttachment', 'RichDocument', 'LinguaPlone'])
+        setupPloneSite(products=['osha.theme', 'SimpleAttachment', 'RichDocument', 'LinguaPlone'])
         import Products.PlacelessTranslationService
+        import osha.theme
         zcml.load_config('configure.zcml', Products.PlacelessTranslationService)
+        zcml.load_config('configure.zcml', osha.theme)
         fiveconfigure.debug_mode = False
         ztc.installPackage('Products.PlacelessTranslationService', quiet=True)
+        ztc.installPackage('osha.theme')
         SiteLayer.setUp()
-
-@onsetup
-def setup_product():
-    """Set up additional products and ZCML required to test this product.
-    """
-    fiveconfigure.debug_mode = True
-    import osha.theme
-    zcml.load_config('configure.zcml', osha.theme)
-    fiveconfigure.debug_mode = False
-    ztc.installPackage('osha.theme')
-
-setup_product()
-setupPloneSite(products=['osha.theme'])
 
 class OshaThemeTestCase(PloneTestCase):
     """Base class for integration tests for the 'OshaTheme' product.
