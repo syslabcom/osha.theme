0c4e9b60f9bdd0887bfe9e23aab2f5f0e6372a72
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-09 14:05:55 +0000

    Remove the linguatools doctest. It wil be added to the slc.linguatools egg

--- src/osha/theme/tests/test_doctests.py
+++ src/osha/theme/tests/test_doctests.py
@@ -1,25 +1,16 @@
-import os, sys
-
-import glob
 import doctest
 import unittest
-from Globals import package_home
-from base import OshaThemeFunctionalTestCase
-from Testing.ZopeTestCase import FunctionalDocFileSuite as Suite
 
+from base import OshaThemeFunctionalTestCase
+
+from Testing.ZopeTestCase import FunctionalDocFileSuite as Suite
 
 OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
                doctest.ELLIPSIS |
                doctest.NORMALIZE_WHITESPACE)
 
-
 def test_suite():
     return unittest.TestSuite((
-
-            Suite('tests/linguatest.txt',
-                   optionflags=OPTIONFLAGS,
-                   package='osha.theme',
-                   test_class=OshaThemeFunctionalTestCase) ,
 
             Suite('tests/mytest.txt',
                    optionflags=OPTIONFLAGS,
