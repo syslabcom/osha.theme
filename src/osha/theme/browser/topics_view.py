a061b44953ccaafa447417b1edb236e52906c3df
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-16 15:08:11 +0000

    Adjusted the topic view to use related images or video and the body of a document instead of using folder content to display the topic and topics page views

--- src/osha/theme/browser/topics_view.py
+++ src/osha/theme/browser/topics_view.py
@@ -1,8 +1,38 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
+from p4a.plonevideoembed.interfaces import IVideoLinkEnhanced
+from Products.ATContentTypes.interface.image import IATImage
 
-class TopicsView(BrowserView):
+class TopicsBrowserView(BrowserView):
+
+    images = []
+    slideswitch_template = ViewPageTemplateFile('templates/slideswitch.pt')
+    image_scale = "mini"
+
+    def getRelatedMedia(self, video_width, image_scale):
+        """ Return a video, slideshow or image.
+        """
+        context = self.context
+        related_items = context.getRelatedItems()
+        videos = [i for i in related_items if
+                  IVideoLinkEnhanced.providedBy(i)]
+        self.image_scale = image_scale
+        video_width = int(video_width)
+        if videos:
+            video = videos[0]
+            embed = video.restrictedTraverse("@@video-embed.htm")
+            return embed.get_code(width=video_width)
+        elif related_items:
+            self.images = [i for i in related_items if
+                           IATImage.providedBy(i)]
+            if len(self.images) == 1:
+                image = self.images[0]
+                return image.tag(scale=image_scale)
+            else:
+                return self.slideswitch_template()    
+
+class TopicsView(TopicsBrowserView):
     """ View class for /topics
     """
     template = ViewPageTemplateFile('templates/topics_view.pt')
@@ -23,7 +53,7 @@ class TopicsView(BrowserView):
                                 'path': path})
         return images
 
-class TopicView(BrowserView):
+class TopicView(TopicsBrowserView):
     """ View class for /topics/topic
     """
     template = ViewPageTemplateFile('templates/topic_view.pt')
