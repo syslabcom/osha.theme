80cd463452c40e3c2d4c2816bf13e420289f8f98
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2010-01-18 17:44:22 +0000

    Add the AnnotatableLinkListVocabulary again, since we need to make a release without osha.adaptation.

--- src/osha/theme/vocabulary.py
+++ src/osha/theme/vocabulary.py
@@ -5,7 +5,10 @@ from zope.schema.vocabulary import SimpleVocabulary
 
 from zope.app.component.hooks import getSite
 
+from Products.Archetypes.atapi import DisplayList
+from Products.Archetypes.interfaces._vocabulary import IVocabulary
 from Products.CMFCore.utils import getToolByName
+
 
 class SubjectValuesVocabulary(object):
     """ Vocabulary factory returning all available index values for the
@@ -56,4 +59,46 @@ class PressContactVocabulary(object):
         return SimpleVocabulary(items)
 
 PressContactVocabulary = PressContactVocabulary()
+
+
+# XXX: This needs to be removed before osha.adaptation integration
+class AnnotatableLinkListVocabulary(object):
+    """Vocabulary factory returning Section names for the AnnotatableLinkList Mechanism in the Document
+    """
+    implements(IVocabulary)
+
+    display_list = [
+        ("authorities", "Authorities"), 
+        ("social_partners", "Social Partners"),
+        ("research_organisations", "Research Organisations"), 
+        ("other_national", "Other National Sites"), 
+        ("more", "More Related Content"),
+    ]
+
+
+    def getDisplayList(self, context=None):
+        """ """
+        site = getSite()
+        # perhaps check context for some settings, otherwise return a default
+        return DisplayList(self.display_list)
+
+    def getVocabularyDict(self, instance=None):
+        """ """
+        d = {}
+        for i in self.display_list:
+            d[i[0]] = i[1]
+        return d
+
+    def isFlat(self):
+        """ returns true if the underlying vocabulary is flat, otherwise
+            if its hierachical (tree-like) it returns false.
+        """
+        return True
+
+    def showLeafsOnly(self):
+        """ returns true for flat vocabularies. In hierachical (tree-like)
+            vocabularies it defines if only leafs should be displayed, or
+            knots and leafs.
+        """
+        return True
 
