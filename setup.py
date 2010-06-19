66a8de9b763dbb00c9353dfdcd3a3945c28618ab
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2010-06-19 10:38:36 +0000

    Back to development: 1.3.26

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.3.25'
+version = '1.3.26dev'
 
 long_description = (
     read('README.txt')
