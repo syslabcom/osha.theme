c1f9bf9e39db0b23e3468464caa8703d1c6d38a7
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2010-07-04 22:41:01 +0000

    Replacing urls which depend on the context of the target object with it's proper url.
    This fixes the issue with broken images in news items and seems to
    treat links correctly etc. I tried to parse the view, extract just the
    elements of interest and convert the result to html
    again. lxml.etree.fromstring complains about invalid entities
    e.g. mdash, and parsing with the lxml soupparser and then converting
    back to html with lxml.etree.tostring breaks the layout
    significantly.

--- src/osha/theme/browser/telescope.py
+++ src/osha/theme/browser/telescope.py
@@ -1,7 +1,10 @@
 import logging
+
 from Acquisition import aq_base, aq_inner, aq_acquire
+
 from Products.Five.browser import BrowserView
 from zope.publisher.interfaces import NotFound
+from zope.component import getMultiAdapter
 
 logger = logging.getLogger('osha.theme/browser/telescope.py')
 
@@ -10,9 +13,10 @@ class TelescopeView(BrowserView):
     Renders an object in a different location of the site when passed the
     path to it #1150
     """
+
     def __call__(self, *args, **kw):
         """
-        Uses aq_aqcuire to return the specified page template
+        Uses ac_aqcuire to return the specified page template
         """
         if self.request.has_key("path"):
             path = self.request["path"]
@@ -23,18 +27,37 @@ class TelescopeView(BrowserView):
                 logger.log(logging.INFO, "Invalid path: %s" %path)
                 raise NotFound
             if target_obj:
+                context = self.context
+                request = self.request
+                view = ""
                 try:
                     # Strip the target_obj of context with aq_base.
                     # Put the target in the context of self.context.
                     # getDefaultLayout returns the name of the default
                     # view method from the factory type information
-                    return aq_acquire(aq_base(target_obj).__of__(self.context),
-                                      target_obj.getDefaultLayout())(**kw)
+                    view = aq_acquire(aq_base(target_obj).__of__(context),
+                                       target_obj.getDefaultLayout())(**kw)
+
                 except AttributeError, message:
                     logger.log(logging.ERROR,
                                "Error acquiring template: %s, path: %s"\
                                %(message, path))
                     raise NotFound
+                if view:
+                    # All images and links in the target document
+                    # which depend on the context e.g. images in News
+                    # items, will be broken, so we do a simple replace
+                    # to fix them.
+
+                    # I tried to be a bit more clever about this and
+                    # use the lxml soupparser, but it breaks the
+                    # layout severely when it is converted back to
+                    # html (deroiste)
+                    proxy_url = context.absolute_url() + "/" +\
+                                 target_obj.getId()
+                    target_url = target_obj.absolute_url()
+                    return view.replace(proxy_url, target_url)
+
         else:
             logger.log(logging.INFO, "No path specified")
             raise NotFound
