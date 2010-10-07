2c6c0b113e13bd30b56cd93ae27a29e5f0995a7b
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2010-10-07 00:15:46 +0000

    Preparing release 1.3.34

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.3.34dev'
+version = '1.3.34'
 
 long_description = (
     read('README.txt')
