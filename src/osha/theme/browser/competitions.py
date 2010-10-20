01a9040b8abd4ad4fdf9769c4c1a96903cc59f99
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2010-10-20 15:44:55 +0000

    return up to 3 image paths

--- src/osha/theme/browser/competitions.py
+++ src/osha/theme/browser/competitions.py
@@ -29,38 +29,54 @@ class CompetitionsView(BrowserView):
         self.path = '/'.join(self.parent.getPhysicalPath())
 
     def getUpcoming(self):
-        context = self.context
-        upcoming = list()
-        catalog = getToolByName(context, 'portal_catalog')
-        res = catalog(portal_type='Folder', path=self.path,
+        """ Get upcoming competitions """
+        query = dict(portal_type='Folder', path=self.path,
             review_state='published',
             effective=dict(query=self.now, range='min'))
+        return self._getCompetitions(query)
+
+    def getOngoing(self):
+        """ Get ongoing competitions """
+        query = dict(portal_type='Folder', path=self.path,
+            review_state='published',
+            effective=dict(query=self.now, range='max'),
+            expires=dict(query=self.now, range='min'))
+        return self._getCompetitions(query)
+
+    def _getCompetitions(self, query):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        competitions = list()
+        res = catalog(query)
         for r in res:
             folder = r.getObject()
             img = ''
             item = folder
             default_view = getattr(folder, folder.defaultView())
             if IBaseContent.providedBy(default_view):
-                img = self.getRelatedImage(default_view)
+                images = self.getRelatedImages(default_view, 'mini')
                 item = default_view
-            upcoming.append(dict(item=item, img=img))
-        return upcoming
+            competitions.append(dict(item=item, images=images))
+        return competitions
 
-    def getRelatedImage(self, obj):
+    def getRelatedImages(self, obj, image_scale):
         """
         Return html for a video, slideshow or image from the related
         items of the canonical translation.
         """
+        scaled = list()
         obj = obj.getCanonical()
         related_items = obj.getRelatedItems()
         images = [i for i in related_items if IATImage.providedBy(i)]
-        if len(images) > 0:
-            image = images[0]
-            return image.absolute_url()
-        return ''
+        # Limit to three
+        images = images[:3]
+        for image in images:
+            scaled_img = image.getField('image').getScale(image,
+                scale=image_scale)
+            scaled.append(scaled_img.absolute_url())
+        return scaled
 
 
-class CompetitionDetail(BrowserView):
+class CompetitionDetail(CompetitionsView):
     implements(ICompetitionDetail)
 
     template = ViewPageTemplateFile('templates/competition_detail.pt')
@@ -68,3 +84,7 @@ class CompetitionDetail(BrowserView):
 
     def __call__(self):
         return self.template()
+
+    def getTeaserImage(self):
+        images = self.getRelatedImages(self.context, 'mini')
+        return len(images) and images[0] or ''
