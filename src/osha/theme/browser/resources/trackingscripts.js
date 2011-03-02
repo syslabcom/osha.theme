057f3420702aab81b32534db0757aee0832652e4
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-03-02 10:27:24 +0000

    In the javascript to track publication downloads, eliminate "http://osha.europa.eu"

--- src/osha/theme/browser/resources/trackingscripts.js
+++ src/osha/theme/browser/resources/trackingscripts.js
@@ -2,7 +2,7 @@ jq(document).ready(
     function(){
         jq('.publicationURL a').bind('click', function(event){
         $url = this['href'];
-        $url=$url.replace(/(http:\/\/[\S]+?)\/(\S|$)/gim, '$1/Events/PDF/$2');
+        $url=$url.replace(/(http:\/\/[\S]+?)\/(\S|$)/gim, '/Events/PDF/$2');
         $url = $url.replace('/at_download/file', '');
         pageTracker._trackPageview($url);
         return true;
