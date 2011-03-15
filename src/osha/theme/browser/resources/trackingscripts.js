f645f00a3f0af7bf96feccf5563e5f17d9972088
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-03-15 14:00:09 +0000

    replaced "jq" with "jQuery"

--- src/osha/theme/browser/resources/trackingscripts.js
+++ src/osha/theme/browser/resources/trackingscripts.js
@@ -1,6 +1,6 @@
-jq(document).ready(
+jQuery(document).ready(
     function(){
-        jq('.publicationURL a').bind('click', function(event){
+        jQuery('.publicationURL a').bind('click', function(event){
         $url = this['href'];
         $url=$url.replace(/(http:\/\/[\S]+?)\/(\S|$)/gim, '/Events/PDF/$2');
         $url = $url.replace('/at_download/file', '');
