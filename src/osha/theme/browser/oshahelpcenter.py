77360877b4e67fe6932fc20bace0ec6658ca4e88
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2011-05-06 14:06:10 +0000

    Adding a custom view for the FAQ section #2385

--- src/osha/theme/browser/oshahelpcenter.py
+++ src/osha/theme/browser/oshahelpcenter.py
@@ -13,55 +13,31 @@ class OSHAHelpCenterView(BrowserView):
     def __call__(self):
         return self.template()
 
-    def get_subject_counts(self):
-        """get the Subject/Subcategory for all FAQ objects in this area
-
-        Since we get them all anyway, we might as well count the
-        number of items in each category, also get the translation
-        from the translation for Subject
-        {'young_people': 21, 'disability': 27, ...} """
-        portal = getSite()
-
-        pc = portal.portal_catalog
-        path = "/".join(self.context.getPhysicalPath())
-        faqs = pc.searchResults({"portal_type":"HelpCenterFAQ",
-                                 "path":path})
-
-        subjects = {}
-        for faq in faqs:
-            for subject in faq.Subject:
-                count = subjects.setdefault(subject, 0)
-                subjects[subject] = count + 1
-        return subjects
-
     def get_vocab_dict(self):
         portal = getSite()
         pv = portal.portal_vocabularies
         vocab = pv.Subcategory
         return vocab.getVocabularyDict(self.context)
 
-    def get_subject_list(self):
-        """get a list of Subjects/Subcategories
+    def get_faqs_in_selected_category(self):
+        portal = getSite()
+        pc = portal.portal_catalog
+        query = {}
+        query["portal_type"] = "HelpCenterFAQ"
 
-        Sorted by the translated term
-        [{"title":"Accident Prevention",
-          "key":"accident_prevention",
-          "count": 10}, ...] """
-        vocab_dict = self.get_vocab_dict()
-        subjects = self.get_subject_counts()
-        subject_list = []
-        for subject in subjects:
-            subject_list.append({"title":vocab_dict[subject][0],
-                                 "key":subject,
-                                 "count":subjects[subject]})
-        return sorted(subject_list, key=lambda x: x["title"])
+        form = self.request.form
 
-    def get_sub_subject_list(self, subject):
-        """get a list of sub-subject from the vocabulary"""
-        vocab_dict = self.get_vocab_dict()
-        sub_subjects = vocab_dict[subject][1]
-        sub_subject_list = []
-        for sub_subject in sub_subjects:
-            sub_subject_list.append({"title":sub_subjects[subject][0],
-                                     "key":subject})
+        searchable_text = form.get("SearchableText", "")
+        if searchable_text != "":
+            query["SearchableText"] = searchable_text
 
+        subcategory = form.get("subcategory", "") or form.get("category", "")
+        if subcategory:
+            query["subcategory"] = subcategory
+
+        if searchable_text == subcategory == "":
+            path = "/".join(self.context.getPhysicalPath())
+            query["path"] = path+"/general-information"
+
+        faqs = pc.searchResults(query)
+        return faqs
