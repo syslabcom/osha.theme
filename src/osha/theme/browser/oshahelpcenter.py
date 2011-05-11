97f82221292cc19de492fc26a00075c0083af4d7
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2011-05-11 16:42:45 +0000

    Osha FAQs: Only displaying categories which are in use, and layout tweaks as requested refs #2385

--- src/osha/theme/browser/oshahelpcenter.py
+++ src/osha/theme/browser/oshahelpcenter.py
@@ -1,5 +1,5 @@
 from zope.app.component.hooks import getSite
-
+from ordereddict import OrderedDict
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
@@ -10,28 +10,31 @@ class OSHAHelpCenterView(BrowserView):
     template = ViewPageTemplateFile('templates/osha_help_center_view.pt')
     template.id = "osha_help_center_view"
 
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.faq_form = self.request.form
+        self.category = self.faq_form.get("category", "")
+        self.portal = getSite()
+        self.pc = self.portal.portal_catalog
+        self.faqs = self.get_faqs()
+
+        subcategory_vocab = self.portal.portal_vocabularies.Subcategory
+        self.vocab_dict = subcategory_vocab.getVocabularyDict(self.context)
+
     def __call__(self):
         return self.template()
 
-    def get_vocab_dict(self):
-        portal = getSite()
-        pv = portal.portal_vocabularies
-        vocab = pv.Subcategory
-        return vocab.getVocabularyDict(self.context)
-
-    def get_faqs_in_selected_category(self):
-        portal = getSite()
-        pc = portal.portal_catalog
+    def get_faqs(self):
+        """ Return a list of FAQ objects. """
         query = {}
         query["portal_type"] = "HelpCenterFAQ"
 
-        form = self.request.form
-
-        searchable_text = form.get("SearchableText", "")
+        searchable_text = self.faq_form.get("SearchableText", "")
         if searchable_text != "":
             query["SearchableText"] = searchable_text
 
-        subcategory = form.get("subcategory", "") or form.get("category", "")
+        subcategory = self.faq_form.get("subcategory", "")  or self.category
         if subcategory:
             query["subcategory"] = subcategory
 
@@ -39,5 +42,66 @@ class OSHAHelpCenterView(BrowserView):
             path = "/".join(self.context.getPhysicalPath())
             query["path"] = path+"/general-information"
 
-        faqs = pc.searchResults(query)
+        faq_brains = self.pc.searchResults(query)
+        faqs = [i.getObject() for i in faq_brains]
         return faqs
+
+    def _get_relevant_categories(self, faqs, vocab_dict):
+        """We are only interested either in the categories at the top
+        level or below the current category. We also only want
+        categories which are being used."""
+        if self.category:
+            category_attr = "subcategory"
+        else:
+            category_attr = "Subject"
+        possible_categories = vocab_dict.keys()
+
+        faq_categories = set()
+        for faq in faqs:
+            for category in getattr(faq, category_attr):
+                faq_categories.add(category)
+        return faq_categories.intersection(possible_categories)
+
+    def _sort_categories_by_title(self, categories, vocab_dict):
+        """ Return a list of category ids, sorted by the title. The
+        title of an item in a vocab_dict is the first element of the
+        tuple. """
+        category_titles = []
+        for category in categories:
+            title = vocab_dict[category][0]
+            category_titles.append((category, title))
+        sorted_categories = sorted(category_titles, key=lambda x: x[1])
+        return [i[0] for i in sorted_categories]
+
+    def _get_categories(self, faqs, vocab_dict):
+        """ Return a sorted vocab_dict of relevant categories. """
+        relevant_categories = self._get_relevant_categories(faqs, vocab_dict)
+
+        sorted_categories = self._sort_categories_by_title(
+            relevant_categories, vocab_dict)
+
+        category_dict = OrderedDict()
+        for category in sorted_categories:
+            category_dict[category] = vocab_dict[category]
+
+        return category_dict
+
+    def get_categories(self):
+        """ The "subcategory" field is not stored in the metadata but
+        the top level "subcategory" is indexed as "Subject". We take
+        advantage of this to get a list of all the top level
+        "subcategory" values which are actually in use.
+        """
+        if self.category:
+            return []
+        faq_brains = self.pc.searchResults({"portal_type": "HelpCenterFAQ"})
+        return self._get_categories(faq_brains, self.vocab_dict)
+
+    def get_subcategories(self, category):
+        """ We already have self.faqs for the selected category so we
+        get a list of "subcategory" values directly from these faq
+        objects. """
+        subcat_vocab_dict = self.vocab_dict[category][1]
+        if not subcat_vocab_dict:
+            return []
+        return self._get_categories(self.faqs, subcat_vocab_dict)
