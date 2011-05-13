537101805a176ceacdf854ddb3dc85df4a09d4b8
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2011-05-13 14:30:15 +0000

    Some changes to base.py to get tests working. Works now, but base.py not yet finished necessarily (doesn't install all add-ons)

--- src/osha/theme/tests/base.py
+++ src/osha/theme/tests/base.py
@@ -1,100 +1,112 @@
-from StringIO import StringIO
 import os
-
 from Globals import package_home
-from Products.Five import fiveconfigure
-from Products.Five import zcml
+from StringIO import StringIO
 from Testing import ZopeTestCase as ztc
 
-from Products.PloneTestCase import layer
-from Products.PloneTestCase.layer import onsetup, PloneSite
-
-# importing LinguaPlone to avoid a problem with circular imports
-# See http://do3cc.blogspot.com/2010/08/dont-catch-import-errors-use.html
-# The problem begins here:
-# Products/PloneTestCase/setup.py:
-# ZopeTestCase.installProduct('CMFPlone', quiet=1)
-import Products.LinguaPlone
-from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
-
-from Products.PloneTestCase.PloneTestCase import PloneTestCase
-from Products.PloneTestCase.PloneTestCase import setupPloneSite
-
-
 from plone.browserlayer import utils as browserlayerutils
-from osha.policy.interfaces import IOSHACommentsLayer
 
+from Products.PloneTestCase import layer
+from Products.PloneTestCase import PloneTestCase as ptc
+
+# # importing LinguaPlone to avoid a problem with circular imports
+# # See http://do3cc.blogspot.com/2010/08/dont-catch-import-errors-use.html
+# # The problem begins here:
+# # Products/PloneTestCase/setup.py:
+# # ZopeTestCase.installProduct('CMFPlone', quiet=1)
+# import Products.LinguaPlone
+
+from osha.policy.interfaces import IOSHACommentsLayer
 from osha.theme.config import product_globals
 
 
 SiteLayer = layer.PloneSite
 
 class OshaThemeLayer(SiteLayer):
+
     @classmethod
     def setUp(cls):
+        """ The osha.theme package depends on osha.policy so the packages imported
+            by the osha.policy tests base.py are also imported and configured here.
         """
-        The osha.theme package depends on osha.policy so the packages imported
-        by the osha.policy tests base.py are also imported and configured here.
-        """
-        ztc.installProduct('PloneLanguageTool')
-        ztc.installProduct('LinguaPlone')
-        ztc.installProduct('SimpleAttachment')
-        ztc.installProduct('RichDocument')
-        ztc.installProduct('PlacelessTranslationService')
-        ztc.installPackage('osha.policy')
-        setupPloneSite(products=[
-                                'slc.shoppinglist',
-                                'CMFLinkChecker',
-                                'LinguaPlone',
-                                'slc.linguatools',
-                                'osha.policy',
-                                'osha.theme',
-                                'p4a.subtyper',
-                                'RichDocument',
-                                'SimpleAttachment',
-                                ])
-        import Products.PlacelessTranslationService
-        import osha.theme
-        import osha.policy
-        import p4a.subtyper
-        import slc.shoppinglist
-        import slc.linguatools
 
-        # osha.policy related products:
-        ztc.installPackage('osha.legislation')
-        ztc.installPackage('slc.seminarportal')
-        ztc.installPackage('slc.alertservice')
-        ztc.installPackage('slc.linguatools')
         ztc.installProduct('ATCountryWidget')
         ztc.installProduct('ATVocabularyManager')
         ztc.installProduct('LinguaPlone')
         ztc.installProduct('ProxyIndex')
         ztc.installProduct('Relations')
         ztc.installProduct('TextIndexNG3')
+        
+        ztc.installPackage('osha.legislation')
 
-        zcml.load_config('configure.zcml', Products.PlacelessTranslationService)
-        zcml.load_config('configure.zcml', osha.theme)
-        zcml.load_config('configure.zcml', osha.policy)
-        zcml.load_config('configure.zcml', p4a.subtyper)
-        zcml.load_config('configure.zcml', slc.shoppinglist)
-        zcml.load_config('configure.zcml', slc.linguatools)
-        fiveconfigure.debug_mode = False
-        ztc.installPackage('Products.PlacelessTranslationService', quiet=True)
-        ztc.installPackage('osha.theme')
-        ztc.installPackage('osha.policy')
-        ztc.installPackage('slc.shoppinglist')
-        ztc.installPackage('p4a.subtyper')
-        # register the Browserlayer from osha.policy, so that our schema-extensions
-        # using IBrowserLayerAwareExtender work
+        PRODUCTS = [
+                'osha.policy',
+                ]
+        ptc.setupPloneSite(products=PRODUCTS)
         browserlayerutils.register_layer(IOSHACommentsLayer, 'osha.policy')
         SiteLayer.setUp()
 
-class OshaThemeTestCase(PloneTestCase):
+        # import Products.PlacelessTranslationService
+        # import osha.policy
+        # import osha.theme
+        # import p4a.subtyper
+        # import slc.linguatools
+        # import slc.shoppinglist
+
+        # ztc.installProduct('PloneLanguageTool')
+        # ztc.installProduct('LinguaPlone')
+        # ztc.installProduct('SimpleAttachment')
+        # ztc.installProduct('RichDocument')
+        # ztc.installProduct('PlacelessTranslationService')
+        # ptc.setupPloneSite(products=[
+        #                         'slc.shoppinglist',
+        #                         'CMFLinkChecker',
+        #                         'LinguaPlone',
+        #                         'slc.linguatools',
+        #                         'osha.policy',
+        #                         'osha.theme',
+        #                         'RichDocument',
+        #                         'SimpleAttachment',
+        #                         ])
+
+        # # osha.policy related products:
+        # ztc.installPackage('slc.seminarportal')
+        # ztc.installPackage('slc.alertservice')
+        # ztc.installPackage('slc.linguatools')
+        # ztc.installProduct('ATCountryWidget')
+        # ztc.installProduct('ATVocabularyManager')
+        # ztc.installProduct('LinguaPlone')
+        # ztc.installProduct('ProxyIndex')
+        # ztc.installProduct('Relations')
+        # ztc.installProduct('TextIndexNG3')
+
+        # zcml.load_config('configure.zcml', Products.LinguaPlone)
+        # zcml.load_config('configure.zcml', Products.PlacelessTranslationService)
+        # zcml.load_config('configure.zcml', osha.theme)
+        # zcml.load_config('configure.zcml', osha.policy)
+        # zcml.load_config('configure.zcml', p4a.subtyper)
+        # zcml.load_config('configure.zcml', slc.shoppinglist)
+        # zcml.load_config('configure.zcml', slc.linguatools)
+
+        # fiveconfigure.debug_mode = False
+
+        # ztc.installPackage('Products.PlacelessTranslationService', quiet=True)
+        # ztc.installPackage('osha.policy')
+        # ztc.installPackage('osha.policy')
+        # ztc.installPackage('osha.theme')
+        # ztc.installPackage('p4a.subtyper')
+        # ztc.installPackage('slc.shoppinglist')
+
+        # register the Browserlayer from osha.policy, so that our schema-extensions
+        # using IBrowserLayerAwareExtender work
+
+
+class OshaThemeTestCase(ptc.PloneTestCase):
     """Base class for integration tests for the 'OshaTheme' product.
     """
     layer = OshaThemeLayer
 
-class OshaThemeFunctionalTestCase(FunctionalTestCase):
+
+class OshaThemeFunctionalTestCase(ptc.FunctionalTestCase):
     """Base class for functional integration tests for the 'OshaTheme' product.
     """
     layer = OshaThemeLayer
@@ -105,3 +117,4 @@ class OshaThemeFunctionalTestCase(FunctionalTestCase):
         data = StringIO(open(filename, 'r').read())
         data.filename = os.path.basename(rel_filename)
         return data
+
