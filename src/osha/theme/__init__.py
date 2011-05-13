399d2b4bdcb8c1ed66aabed3dd51da84d332ffb7
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2011-05-13 14:28:51 +0000

    Define _ as well

--- src/osha/theme/__init__.py
+++ src/osha/theme/__init__.py
@@ -1,8 +1,7 @@
 from zope.i18nmessageid import MessageFactory
-
 from AccessControl import ModuleSecurityInfo
 
-OSHAMessageFactory = MessageFactory('osha')
+_ = OSHAMessageFactory = MessageFactory('osha')
 
 def initialize(context):
     """Intializer called when used as a Zope 2 product."""
