7baf4bf9336207e3ee42300f825ef76aa2a1defa
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2011-08-17 14:51:26 +0000

    Testing that all the resources used in the napo films section exist on the amazon storage refs #1555

--- src/osha/theme/tests/test_multimedia_views.py
+++ src/osha/theme/tests/test_multimedia_views.py
@@ -1,9 +1,18 @@
+import httplib
 import unittest
+from urlparse import urlparse
 
 from ordereddict import OrderedDict
 
 from osha.theme.browser.multimedia_view import MultimediaFilmEpisodeListingView
 from osha.theme.tests.base import OshaThemeTestCase
+
+
+def resource_exists(url):
+    domain_name = urlparse(url)[1]
+    conn = httplib.HTTPConnection(domain_name)
+    conn.request("HEAD", url)
+    return conn.getresponse().status == 200
 
 
 class TestFilmViewsUnitTests(OshaThemeTestCase):
@@ -12,9 +21,8 @@ class TestFilmViewsUnitTests(OshaThemeTestCase):
         self.loginAsPortalOwner()
         self.portal.invokeFactory("Folder", "multimedia")
         self.film_episode_listing_view = self.portal.unrestrictedTraverse(
-            ("/plone/multimedia"
-             "/multimedia-film-episodes-listing-view"))
-        self.film_episode_listing_view.request.form["film"] = \
+            "/plone/multimedia/multimedia-film-episodes-listing-view")
+        self.film_episode_listing_view.request.form["filmid"] = \
             "napo-015-safe-moves"
 
     def test_javascript_substitution(self):
@@ -42,6 +50,33 @@ class TestFilmViewsUnitTests(OshaThemeTestCase):
                'video_webm': 'http://media.osha.europa.eu/napofilm/movie1.webm'
                })])
         self.assertEquals(defaults, expected_defaults)
+
+    def test_napofilmstructure_remote_files_exist(self):
+        missing_resources = []
+        view = self.film_episode_listing_view
+        films_data = view.set_movie_defaults(view.films_data)
+
+        resource_keys = [
+            "image", "video_mp4", "video_ogv", "video_webm",
+            "video_avi", "video_wmv"]
+
+        for film_id in films_data:
+            film_details = films_data[film_id]
+
+            for resource_key in resource_keys:
+                url = film_details[resource_key]
+                if not resource_exists(url):
+                    missing_resources.append(url)
+            view.request.form["filmid"] = film_id
+            for episode_id in view.episodes.keys():
+                episode = view.episodes[episode_id]
+                for resource_key in resource_keys:
+                    url = episode[resource_key]
+                    if not resource_exists(url):
+                        missing_resources.append(url)
+            self.assertEquals(
+                missing_resources, [],
+                msg="Resources missing: %s" % missing_resources)
 
 
 def test_suite():
