acdd1eca0bbad0391cd8894df71edef6637c66d0
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2012-05-11 13:15:10 +0000

    Reversing the logic, it's easier to read

--- src/osha/theme/browser/multimedia_view.py
+++ src/osha/theme/browser/multimedia_view.py
@@ -16,10 +16,10 @@ class LipstickView(BrowserView):
 
     def __call__(self, *args, **kw):
         view = aq_acquire(self.context, self.context.getDefaultLayout())
-        if "main" not in view.macros.keys():
-            self.main_macro = view.macros["content-core"]
-        else:
+        if "main" in view.macros.keys():
             self.main_macro = view.macros["main"]
+        else:
+            self.main_macro = view.macros["content-core"]
         return self.index()
 
 
