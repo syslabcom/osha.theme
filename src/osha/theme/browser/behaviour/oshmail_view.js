42b6133f877e8d09191c67a83ba596ed1fceeb17
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2012-06-12 13:29:35 +0000

    Rewriting the oshmail js to disable some of the automatic features of fancybox, now we explicitly set the dimension, show the spinner, load the contnet into the div, and hide it before loading the content for the next item, this is much more reliable.

--- src/osha/theme/browser/behaviour/oshmail_view.js
+++ src/osha/theme/browser/behaviour/oshmail_view.js
@@ -12,24 +12,47 @@ OSHMAIL.loadOshmailContent = function () {
         type: 'GET',
         url: this.href,
         success: function(data) {
+            var stuff = jQuery(data).find("#collage");
             jQuery("#oshmail-overlay #collage")
-                .replaceWith(jQuery(data)
-                             .find("#collage"));
+                .replaceWith(function() {
+                    console.log(stuff);
+                    return stuff;
+                });
+            jQuery.fancybox.hideActivity();
         }
     });
 };
 
 jQuery(document).ready(function() {
+    var overlayWidth  = jQuery(window).width() - 100,
+        overlayHeight = jQuery(window).height() - 100;
+    jQuery("#oshmail-overlay #collage")
+        .css({
+            "width"    : overlayHeight,
+            "height"   : overlayWidth,
+            "position" : "absolute",
+            "z-index"  : -10
+        })
     if (jQuery("a[rel=oshmail-fancybox]").length>0) {
-        jQuery("a[rel=oshmail-fancybox]").fancybox({
-            'transitionIn'   : 'elastic',
-            'transitionOut'  : 'elastic',
-            'titlePosition'  : 'over',
-            'overlayOpacity' : 0.7,
-            'overlayColor'   : '#FFF',
-            'showNavArrows'  : false,
-            'onStart'        : OSHMAIL.loadOshmailContent,
-            'content'        : jQuery('#oshmail-overlay'),
-        });
+        jQuery("a[rel=oshmail-fancybox]")
+            .fancybox({
+                'transitionIn'      : 'elastic',
+                'transitionOut'     : 'elastic',
+                'titlePosition'     : 'over',
+                'overlayOpacity'    : 0.7,
+                'overlayColor'      : '#FFF',
+                'showNavArrows'     : false,
+                'autoScale'         : false,
+                'autoDimensions'    : false,
+                'enableKeyboardNav' : false,
+                'width'             : overlayWidth,
+                'height'            : overlayHeight,
+                'content'           : jQuery('#oshmail-overlay'),
+                'onComplete'        : OSHMAIL.loadOshmailContent,
+                'onCleanup'         : function() {
+                    jQuery("#oshmail-overlay #collage")
+                        .css({"opacity" : "0",});
+                },
+            });
     }
 })
