c081df6081948e4f9c951c91348443187e7319ed
reinhard <reinhard@97d050b9-b799-4550-a901-4f04598d4601>
2012-07-02 09:31:50 +0000

    fixed batching, added padding

--- src/osha/theme/browser/utils.py
+++ src/osha/theme/browser/utils.py
@@ -2,9 +2,11 @@ from Products.LinguaPlone.catalog import languageFilter
 from collective.solr.flare import PloneFlare
 from collective.solr.interfaces import ISearch
 from collective.solr.utils import prepareData
+from collective.solr.utils import padResults
 from zope.component import queryUtility
+from zope.component.hooks import getSite
 
-def search_solr(query, **params):
+def search_solr(query, request=None, **params):
     search = queryUtility(ISearch)
     dummy = {}
     languageFilter(dummy)
@@ -12,7 +14,11 @@ def search_solr(query, **params):
     langquery = 'Language:(%s)' % ' OR '.join(dummy['Language'])
     query = '(%s) AND %s' % (query, langquery)
     response = search(query, **params)
+    if request is None:
+        request = getSite().REQUEST
+    response.request = request
     results = response.results()
     for idx, flare in enumerate(results):
-        results[idx] = PloneFlare(flare)
+        results[idx] = PloneFlare(flare, request=request)
+    padResults(results, **params)           # pad the batch
     return response
