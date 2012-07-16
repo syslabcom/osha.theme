85727de80e32673867e2e64101b2d65f899a133b
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2012-07-16 17:36:07 +0000

    Display the quickupload dialog from an action refs #5277

---
+++ src/osha/theme/browser/behaviour/quickupload.js
@@ -0,0 +1,46 @@
+/* Open the collective.quickupload ajax view in an overlay when #contentview-upload (an action) is clicked. 
+
+   The hidden input field is wrapped in a form in the main template: form#quickuploader
+*/
+
+var OSHUPLOAD = {}
+
+OSHUPLOAD.showUploader = function () {
+    // If it gets called again when it's already opened it loads the
+    // content of the page into the element
+    if ( jQuery(".quick-uploader").length < 1 ) {
+
+        jQuery("form#quickuploader").each(function(){
+            var uploadUrl =  jQuery('.uploadUrl', this).val();
+            var uploadData =  jQuery('.uploadData', this).val();
+            var UlDiv = jQuery(this);
+            jQuery.ajax({
+                type: 'GET',
+                url: uploadUrl,
+                data: uploadData,
+                dataType: 'html',
+                contentType: 'text/html; charset=utf-8',
+                success: function(html) {
+                    UlDiv.html(html);
+                } });
+        });
+    };
+};
+
+
+jQuery(document).ready( function () {
+    // workaround this MSIE bug :
+    // https://dev.plone.org/plone/ticket/10894
+    if (jQuery.browser.msie) jQuery("#settings").remove();
+    var Browser = {};
+    Browser.onUploadComplete = function() {
+        window.location.reload();
+    }
+
+    // Disable the action link
+    uploadAction = jQuery("#contentview-upload")
+        .find("a")
+        .removeAttr("href");
+
+    jQuery("#contentview-upload").click(OSHUPLOAD.showUploader);
+})
