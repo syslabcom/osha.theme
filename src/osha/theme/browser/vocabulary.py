6d4dcaa0843181c1a500f67a1c21924f0b31f5cc
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-07-17 10:51:53 +0000

    turned get_vocabulary_path into method that can be imported (so that async indexing can use it)

--- src/osha/theme/browser/vocabulary.py
+++ src/osha/theme/browser/vocabulary.py
@@ -5,46 +5,51 @@ from Products.CMFCore.utils import getToolByName
 from plone.memoize import ram
 from time import time
 
+
+def get_vocabulary_path(obj, fieldName=''):
+    field = obj.getField(fieldName)
+    if not field:
+        return []
+    termUID = field.getRaw(obj)
+    pvt = getToolByName(obj, 'portal_vocabularies')
+    VOCAB = getattr(field, 'vocabulary', None)
+    if VOCAB is None:
+        return []
+    vd = VOCAB.getVocabularyDict(obj)
+
+    parents_map = dict()
+    # list of current parents
+    global cp 
+    cp = list()
+    
+    def _recurseDict_cachekey(method, vocab_dict, level):
+        return (vocab_dict.keys())
+
+#    @ram.cache(_recurseDict_cachekey)
+#    @ram.cache(lambda *args: time() // (60 * 60))
+    def recurseDict(vocab_dict, level):
+        global cp
+        for k in vocab_dict.keys():
+            if level==0: 
+                cp = list()
+            else:
+                cp = cp[:level]
+                parents_map[k] = [x for x in cp]
+            vd = vocab_dict[k][1]
+            if vd:
+                cp.append(k)
+                recurseDict(vd, level+1)
+    recurseDict(vd, 0)
+
+    res = set()
+    for term in termUID:
+        res.update(parents_map.get(term, []) + [term])
+
+    return list(res)
+
+
 class VocabularyPathView(BrowserView):
     """View for calculating complete paths of hierarchical vocabulary entries
     """
     def __call__(self, fieldName=''):
-        
-        field = self.context.getField(fieldName)
-        if not field:
-            return []
-#        import pdb; pdb.set_trace()
-        termUID = field.getRaw(self.context)
-        pvt = getToolByName(self.context, 'portal_vocabularies')
-        VOCAB = getattr(field, 'vocabulary', None)
-        if VOCAB is None:
-            return []
-        vd = VOCAB.getVocabularyDict(self.context)
-
-        parents_map = dict()
-        # list of current parents
-        self.cp = list()
-        
-        def _recurseDict_cachekey(method, vocab_dict, level):
-            return (vocab_dict.keys())
-
-#        @ram.cache(_recurseDict_cachekey)
-#        @ram.cache(lambda *args: time() // (60 * 60))
-        def recurseDict(vocab_dict, level):
-            for k in vocab_dict.keys():
-                if level==0: 
-                    self.cp = list()
-                else:
-                    self.cp = self.cp[:level]
-                    parents_map[k] = [x for x in self.cp]
-                vd = vocab_dict[k][1]
-                if vd:
-                    self.cp.append(k)
-                    recurseDict(vd, level+1)
-        recurseDict(vd, 0)
-
-        res = set()
-        for term in termUID:
-            res.update(parents_map.get(term, []) + [term])
-
-        return list(res)
+       return get_vocabulary_path(self.context, fieldName)
