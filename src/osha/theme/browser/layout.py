5efad7feba06444cc7c4eb66524b133690910c0e
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-07-19 13:38:07 +0000

    add subsite id to body class

--- src/osha/theme/browser/layout.py
+++ src/osha/theme/browser/layout.py
@@ -1,5 +1,6 @@
 from plone.app.layout.globals.layout import LayoutPolicy
 from osha.theme.browser.interfaces import IFullWidth
+from slc.subsite.root import getSubsiteRoot
 
 
 class OSHALayoutPolicy(LayoutPolicy):
@@ -12,4 +13,8 @@ class OSHALayoutPolicy(LayoutPolicy):
         body_class = super(OSHALayoutPolicy, self).bodyClass(template, view)
         if IFullWidth.providedBy(self.context):
             body_class = u"%s full-width" % body_class
+        subsite_path = getSubsiteRoot(self.context)
+        body_class = u"%s subsite-%s" % (body_class, \
+            subsite_path.split('/')[-1])
+
         return body_class
