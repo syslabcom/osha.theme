4870e75d9280b9280fba602353fb57cf0905f2a2
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2012-07-25 11:54:24 +0000

    added subsections to layout

--- src/osha/theme/browser/layout.py
+++ src/osha/theme/browser/layout.py
@@ -1,7 +1,7 @@
 from plone.app.layout.globals.layout import LayoutPolicy
 from osha.theme.browser.interfaces import IFullWidth
+from zope.component import getMultiAdapter
 from slc.subsite.root import getSubsiteRoot
-
 
 class OSHALayoutPolicy(LayoutPolicy):
     """A view that gives access to various layout related functions.
@@ -10,9 +10,20 @@ class OSHALayoutPolicy(LayoutPolicy):
     def bodyClass(self, template, view):
         """Return the CSS class to be used on the body tag,
         with additional classes as required by the OSHA theme"""
+        context = self.context
+        portal_state = getMultiAdapter(
+            (context, self.request), name=u'plone_portal_state')
+        navroot = portal_state.navigation_root()
+        
         body_class = super(OSHALayoutPolicy, self).bodyClass(template, view)
         if IFullWidth.providedBy(self.context):
             body_class = u"%s full-width" % body_class
+
+        contentPath = context.getPhysicalPath()[len(navroot.getPhysicalPath()):]
+        if contentPath:
+            for i in range(len(contentPath)):
+                body_class += " subsection%s-%s" % (i, contentPath[i])
+
         subsite_path = getSubsiteRoot(self.context)
         body_class = u"%s subsite-%s" % (body_class, \
             subsite_path.split('/')[-1])
