d8b586b807288bbf0ce494084bf6ce02701eda90
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2012-08-22 09:11:33 +0000

    Fix the oshmail overlay on IE
    IE8 was crashing (reloading the tab, and eventually showing res://ieframe.dll/acr_error.htm in the location bar) when jQuery replaceWith was used to replace the contents of the overlay element. Using .html worked but the content was displayed behind the overlay in IE8, even though it was being added to the correct part of the DOM. Using .append works correctly, tested in FF, Chrome, IE8 and IE9

--- src/osha/theme/browser/behaviour/oshmail_view.js
+++ src/osha/theme/browser/behaviour/oshmail_view.js
@@ -8,33 +8,43 @@ var OSHMAIL = {};
 
 OSHMAIL.loadOshmailContent = function (linkObj) {
     "use strict";
+
     // We can ignore ?set_language if it is set in the href
     var href = linkObj.href.split("?")[0];
+
+    /* IE8 crashes if replaceWith is used, and appending directly to
+       .osha-overlay.full causes the content to be display behind the
+       overlay (in IE8) even though the DOM looks correct
+    */
+    jQuery(".osha-overlay.full").find("#overlay-content").remove();
+
+    jQuery(".osha-overlay.full").show();
     jQuery("#overlay-spinner").show();
     jQuery.ajax({
-        async: false,
+        async: true,
         type: 'GET',
         url: href + "?ajax_load=1",
         success: function (data) {
-            jQuery("#oshmail-overlay #collage")
-                .replaceWith(jQuery(data).find("#collage"));
-            jQuery("#overlay-spinner").hide();
+            var collage = jQuery(data).find("#collage");
+            jQuery(".osha-overlay.full").append("<span id='overlay-content'/>");
+            jQuery("#overlay-content").append(collage);
+            jQuery("#overlay-spinner").hide(); 
         }
     });
 };
 
 jQuery(document).ready(function () {
     "use strict";
-    if (jQuery("a[rel=oshmail-fancybox]").length > 0) {
-        jQuery("a[rel=oshmail-fancybox]")
+    if (jQuery("a[rel=oshmail-overlay]").length > 0) {
+        jQuery("a[rel=oshmail-overlay]")
             .click(function () {
-                jQuery(".osha-overlay").show("slow");
                 OSHMAIL.loadOshmailContent(this);
                 return false;
             });
     }
+
     jQuery(".osha-overlay-close").click(function () {
-        jQuery(".osha-overlay").hide("slow");
+        jQuery(".osha-overlay.full").hide("slow");
     });
 });
 
