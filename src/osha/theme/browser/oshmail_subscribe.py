4aa3c4a7960ca0d9aa880a5e8690f5245d89a47f
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2012-08-23 16:04:55 +0000

    Tidy up, removing unused imports and variables, replacing portal_status_message with addPortalMessage so that it is styled correctly, wrapping message strings so that they may be translated

---
+++ src/osha/theme/browser/oshmail_subscribe.py
@@ -0,0 +1,62 @@
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import BrowserView
+
+from osha.theme import OSHAMessageFactory as _
+
+class OSHmailSubscribe(BrowserView):
+    """View for displaying oshmail
+    """
+
+    def __call__(self, emailaddress, name=''):
+
+        """ helper method to enable osh mail subscription to anonymous user """
+        ptool = getToolByName(self.context, 'portal_url')
+        portal = ptool.getPortalObject()
+
+        reg_tool = getToolByName(self.context, 'portal_registration')
+        host = getToolByName(self.context, 'MailHost')
+
+        REQUEST = self.request
+        if not emailaddress:
+            emailaddress = REQUEST.get('emailaddress', '')
+        refererstem = REQUEST.get('HTTP_REFERER').split('?')[0]
+        referer = refererstem + '?'
+        qs = REQUEST.get('QUERY_STRING', '')
+        if qs:
+            referer += '?' + qs + '&'
+
+        if not reg_tool.isValidEmail(emailaddress):
+            msg = _(u'You did not enter a valid email address.')
+            self.context.plone_utils.addPortalMessage(msg)
+            return REQUEST.RESPONSE.redirect(referer)
+
+        if REQUEST.has_key('unsubscribe'):
+            return REQUEST.RESPONSE.redirect(
+                self.context.absolute_url() +
+                "/confirm-unsubscription?emailaddress=%s" % (emailaddress))
+        else:
+            mesg = "subscribe ICT-TEST anonymous\n"
+            mssg = _(
+                "Thank you for subscribing to the OSHmail newsletter. You will "
+                "receive an email to confirm your subscription.")
+
+
+        recipient = 'listserv@list.osha.europa.eu'
+
+        sender = emailaddress
+        if name:
+            sender = "%s <%s>" % (name, sender)
+
+        subject = ''
+        try:
+            host.send(mesg, mto=recipient, mfrom=sender, subject=subject)
+        except Exception, e:
+            mssg = _("Your subscription could not be sent. Please try again.")
+            mssg = u"%s %s" %(mssg, e)
+
+        self.context.plone_utils.addPortalMessage(mssg)
+        from slc.alertservice.utils import encodeEmail
+        # this feedbackpage has been added to contain a specific
+        # tracking code for an external company
+        return REQUEST.RESPONSE.redirect(
+            refererstem + "?e=%s" % (encodeEmail(sender)))
