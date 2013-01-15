a82426c87a067429435072d3c15e82b5ae4f1d00
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2013-01-15 10:43:29 +0000

    bugfix for the PDF (publication) download tracking: since we now use https, a hardcoded "http" substitution does not work any more; we need to consider https

--- src/osha/theme/browser/resources/trackingscripts.js
+++ src/osha/theme/browser/resources/trackingscripts.js
@@ -2,7 +2,7 @@ jQuery(document).ready(
     function(){
         jQuery('.publicationURL a').bind('click', function(event){
         $url = this['href'];
-        $url=$url.replace(/(http:\/\/[\S]+?)\/(\S|$)/gim, '/Events/PDF/$2');
+        $url=$url.replace(/(http[s]?:\/\/[\S]+?)\/(\S|$)/gim, '/Events/PDF/$2');
         $url = $url.replace('/at_download/file', '');
         pageTracker._trackPageview($url);
         return true;
