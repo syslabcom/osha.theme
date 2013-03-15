93b47477b5f6906f9eca3226086f9451c911e296
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2013-03-15 10:49:26 +0000

    moved all stuff for inline generation into one separate file, away from oshaview

--- src/osha/theme/browser/oshaview.py
+++ src/osha/theme/browser/oshaview.py
@@ -1,9 +1,7 @@
-import urlparse
-import sys
 from urlparse import urljoin
 from types import *
 import logging
-import urllib
+#import urllib2
 from BeautifulSoup import BeautifulSoup, Tag
 
 import Acquisition
@@ -17,8 +15,6 @@ from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import getSiteEncoding
 from Products.Five import BrowserView
 from Products.validation import validation
-import cssutils
-from lxml import etree
 
 from gocept.linkchecker.utils import retrieveHTML, retrieveSTX
 # from p4a.calendar import interfaces as p4aCalendarInterfaces
@@ -30,7 +26,7 @@ from osha.theme.browser.interfaces import IOSHA
 from osha.theme.browser.osha_properties_controlpanel import \
     PropertiesControlPanelAdapter
 from osha.theme.config import *
-from osha.theme.cssselect import CSSSelector, ExpressionError
+from osha.theme.cssselect import Converter
 
 logger = logging.getLogger("oshaview")
 
@@ -385,18 +381,19 @@ class OSHA(BrowserView):
 
     def inlinestyler(self, data):
         """ calls an external service to integrate styles into tags """
-        service_url = "http://inlinestyler.torchboxapps.com/styler/convert/"
-        params = dict(returnraw='1', source=data.encode('utf8'))
-        req = urllib2.Request(url=service_url, data=urllib.urlencode(params))
-        f = urllib2.urlopen(req)
+        # service_url = "http://inlinestyler.torchboxapps.com/styler/convert/"
+        # params = dict(returnraw='1', source=data.encode('utf8'))
+        # req = urllib2.Request(url=service_url, data=urllib.urlencode(params))
+        # f = urllib2.urlopen(req)
 
-        ret = BeautifulSoup(
-            f.read(), convertEntities=BeautifulSoup.HTML_ENTITIES)
-        return ret.text
-        # converter = Converter()
-        # text = converter.perform(data)
-        # ret = BeautifulSoup(text, convertEntities=BeautifulSoup.HTML_ENTITIES)
+        # ret = BeautifulSoup(
+        #     f.read(), convertEntities=BeautifulSoup.HTML_ENTITIES)
         # return ret.text
+        converter = Converter()
+        text = converter.perform(data)
+        if not isinstance(text, unicode):
+            text = text.decode('utf-8')
+        return text
 
     def collage2table(self, data, u=False):
         """ Takes an html page generated from collage in the oshmail format and
@@ -444,156 +441,3 @@ class OSHA(BrowserView):
         if u:
             return unicode(table)
         return str(table)
-
-
-class Converter(object):
-    """
-    This code was copied from https://github.com/davecranwell/inline-styler
-    (c) Dave Cranwell
-    """
-
-    def __init__(self):
-        self.CSSErrors = []
-        self.CSSUnsupportErrors = dict()
-        self.supportPercentage = 100
-        self.convertedHTML = ""
-
-    def perform(self, sourceHTML, sourceURL=''):
-        aggregateCSS = ""
-        document = etree.HTML(sourceHTML)
-
-        aggregateCSS = ""
-
-        # retrieve CSS rel links from html pasted and aggregate into one string
-        CSSRelSelector = CSSSelector(
-            "link[rel=stylesheet],link[rel=StyleSheet],link[rel=STYLESHEET]")
-        matching = CSSRelSelector.evaluate(document)
-        for element in matching:
-            try:
-                csspath = element.get("href")
-                if len(sourceURL):
-                    if element.get("href").lower().find("http://", 0) < 0:
-                        parsedUrl = urlparse.urlparse(sourceURL)
-                        csspath = urlparse.urljoin(
-                            parsedUrl.scheme + "://" + parsedUrl.hostname,
-                            csspath
-                        )
-                f = urllib.urlopen(csspath)
-                aggregateCSS += ''.join(f.read())
-                element.getparent().remove(element)
-            except:
-                raise IOError(
-                    'The stylesheet ' + element.get("href") + ' could not be '
-                    'found'
-                )
-
-        #include inline style elements
-        print aggregateCSS
-        CSSStyleSelector = CSSSelector("style,Style")
-        matching = CSSStyleSelector.evaluate(document)
-        for element in matching:
-            aggregateCSS += element.text
-            element.getparent().remove(element)
-
-        #convert  document to a style dictionary compatible with etree
-        styledict = self._get_style(document, aggregateCSS)
-
-        #set inline style attribute if not one of the elements not worth
-        #styling
-        ignoreList = ['html', 'head', 'title', 'meta', 'link', 'script']
-        for element, style in styledict.items():
-            if element.tag not in ignoreList:
-                v = style.getCssText(separator=u'')
-                element.set('style', v)
-
-        #convert tree back to plain text html
-        convertedHTML = etree.tostring(
-            document, method="xml", pretty_print=True, encoding='UTF-8')
-        #tedious raw conversion of line breaks.
-        convertedHTML = convertedHTML.replace('&#13;', '')
-
-        return convertedHTML
-
-    def _get_style(self, document, css):
-        view = {}
-        specificities = {}
-        supportratios = {}
-        compliance = dict()
-
-        sheet = cssutils.parseString(css)
-
-        rules = (rule for rule in sheet if rule.type == rule.STYLE_RULE)
-        for rule in rules:
-
-            for selector in rule.selectorList:
-                try:
-                    cssselector = CSSSelector(selector.selectorText)
-                    matching = cssselector.evaluate(document)
-
-                    for element in matching:
-                        # add styles for all matching DOM elements
-                        if element not in view:
-                            # add initial
-                            view[element] = cssutils.css.CSSStyleDeclaration()
-                            specificities[element] = {}
-
-                            # add inline style if present
-                            inlinestyletext = element.get('style')
-                            if inlinestyletext:
-                                inlinestyle = cssutils.css.CSSStyleDeclaration(
-                                    cssText=inlinestyletext)
-                            else:
-                                inlinestyle = None
-                            if inlinestyle:
-                                for p in inlinestyle:
-                                    # set inline style specificity
-                                    view[element].setProperty(p)
-                                    specificities[element][p.name] = (
-                                        1, 0, 0, 0)
-
-                        for p in rule.style:
-                            #create supportratio dic item for this property
-                            if p.name not in supportratios:
-                                supportratios[p.name] = {
-                                    'usage': 0, 'failedClients': 0}
-                            #increment usage
-                            supportratios[p.name]['usage'] += 1
-
-                            try:
-                                if not p.name in self.CSSUnsupportErrors:
-                                    for client, support in \
-                                            compliance[p.name].items():
-                                        if support == "N" or support == "P":
-                                            #increment client failure count for
-                                            #this property
-                                            supportratios[p.name]['failedClients'] += 1
-                                            if not p.name in self.CSSUnsupportErrors:
-                                                if support == "P":
-                                                    self.CSSUnsupportErrors[p.name] = [client + ' (partial support)']
-                                                else:
-                                                    self.CSSUnsupportErrors[p.name] = [client]
-                                            else:
-                                                if support == "P":
-                                                    self.CSSUnsupportErrors[p.name].append(client + ' (partial support)')
-                                                else:
-                                                    self.CSSUnsupportErrors[p.name].append(client)
-
-                            except KeyError:
-                                pass
-
-                            # update styles
-                            if p not in view[element]:
-                                view[element].setProperty(
-                                    p.name, p.value, p.priority)
-                                specificities[element][p.name] = selector.specificity
-                            else:
-                                sameprio = (p.priority == view[element].getPropertyPriority(p.name))
-                                if not sameprio and bool(p.priority) or (sameprio and selector.specificity >= specificities[element][p.name]):
-                                    # later, more specific or higher prio
-                                    view[element].setProperty(p.name, p.value, p.priority)
-
-                except ExpressionError:
-                    if str(sys.exc_info()[1]) not in self.CSSErrors:
-                        self.CSSErrors.append(str(sys.exc_info()[1]))
-                    pass
-        return view
--- src/osha/theme/cssselect.py
+++ src/osha/theme/cssselect.py
@@ -7,8 +7,10 @@ This code was copied from https://github.com/davecranwell/inline-styler
 (c) Dave Cranwell
 """
 
-
-
+import cssutils
+import urlparse
+import urllib
+import sys
 import re
 from lxml import etree
 
@@ -664,7 +666,6 @@ def parse(string):
     try:
         return parse_selector_group(stream)
     except SelectorSyntaxError:
-        import sys
         e = sys.exc_info()[1]
         e.args = tuple(["%s at %s -> %s" % (
             e, stream.used, list(stream))])
@@ -984,3 +985,156 @@ class TokenStream(object):
                 return None
             self._peeking = True
         return self.peeked
+
+
+class Converter(object):
+    """
+    This code was copied from https://github.com/davecranwell/inline-styler
+    (c) Dave Cranwell
+    """
+
+    def __init__(self):
+        self.CSSErrors = []
+        self.CSSUnsupportErrors = dict()
+        self.supportPercentage = 100
+        self.convertedHTML = ""
+
+    def perform(self, sourceHTML, sourceURL=''):
+        aggregateCSS = ""
+        document = etree.HTML(sourceHTML)
+
+        aggregateCSS = ""
+
+        # retrieve CSS rel links from html pasted and aggregate into one string
+        CSSRelSelector = CSSSelector(
+            "link[rel=stylesheet],link[rel=StyleSheet],link[rel=STYLESHEET]")
+        matching = CSSRelSelector.evaluate(document)
+        for element in matching:
+            try:
+                csspath = element.get("href")
+                if len(sourceURL):
+                    if element.get("href").lower().find("http://", 0) < 0:
+                        parsedUrl = urlparse.urlparse(sourceURL)
+                        csspath = urlparse.urljoin(
+                            parsedUrl.scheme + "://" + parsedUrl.hostname,
+                            csspath
+                        )
+                f = urllib.urlopen(csspath)
+                aggregateCSS += ''.join(f.read())
+                element.getparent().remove(element)
+            except:
+                raise IOError(
+                    'The stylesheet ' + element.get("href") + ' could not be '
+                    'found'
+                )
+
+        #include inline style elements
+        print aggregateCSS
+        CSSStyleSelector = CSSSelector("style,Style")
+        matching = CSSStyleSelector.evaluate(document)
+        for element in matching:
+            aggregateCSS += element.text
+            element.getparent().remove(element)
+
+        #convert  document to a style dictionary compatible with etree
+        styledict = self._get_style(document, aggregateCSS)
+
+        #set inline style attribute if not one of the elements not worth
+        #styling
+        ignoreList = ['html', 'head', 'title', 'meta', 'link', 'script']
+        for element, style in styledict.items():
+            if element.tag not in ignoreList:
+                v = style.getCssText(separator=u'')
+                element.set('style', v)
+
+        #convert tree back to plain text html
+        convertedHTML = etree.tostring(
+            document, method="xml", pretty_print=True, encoding='UTF-8')
+        #tedious raw conversion of line breaks.
+        convertedHTML = convertedHTML.replace('&#13;', '')
+
+        return convertedHTML
+
+    def _get_style(self, document, css):
+        view = {}
+        specificities = {}
+        supportratios = {}
+        compliance = dict()
+
+        sheet = cssutils.parseString(css)
+
+        rules = (rule for rule in sheet if rule.type == rule.STYLE_RULE)
+        for rule in rules:
+
+            for selector in rule.selectorList:
+                try:
+                    cssselector = CSSSelector(selector.selectorText)
+                    matching = cssselector.evaluate(document)
+
+                    for element in matching:
+                        # add styles for all matching DOM elements
+                        if element not in view:
+                            # add initial
+                            view[element] = cssutils.css.CSSStyleDeclaration()
+                            specificities[element] = {}
+
+                            # add inline style if present
+                            inlinestyletext = element.get('style')
+                            if inlinestyletext:
+                                inlinestyle = cssutils.css.CSSStyleDeclaration(
+                                    cssText=inlinestyletext)
+                            else:
+                                inlinestyle = None
+                            if inlinestyle:
+                                for p in inlinestyle:
+                                    # set inline style specificity
+                                    view[element].setProperty(p)
+                                    specificities[element][p.name] = (
+                                        1, 0, 0, 0)
+
+                        for p in rule.style:
+                            #create supportratio dic item for this property
+                            if p.name not in supportratios:
+                                supportratios[p.name] = {
+                                    'usage': 0, 'failedClients': 0}
+                            #increment usage
+                            supportratios[p.name]['usage'] += 1
+
+                            try:
+                                if not p.name in self.CSSUnsupportErrors:
+                                    for client, support in \
+                                            compliance[p.name].items():
+                                        if support == "N" or support == "P":
+                                            #increment client failure count for
+                                            #this property
+                                            supportratios[p.name]['failedClients'] += 1
+                                            if not p.name in self.CSSUnsupportErrors:
+                                                if support == "P":
+                                                    self.CSSUnsupportErrors[p.name] = [client + ' (partial support)']
+                                                else:
+                                                    self.CSSUnsupportErrors[p.name] = [client]
+                                            else:
+                                                if support == "P":
+                                                    self.CSSUnsupportErrors[p.name].append(client + ' (partial support)')
+                                                else:
+                                                    self.CSSUnsupportErrors[p.name].append(client)
+
+                            except KeyError:
+                                pass
+
+                            # update styles
+                            if p not in view[element]:
+                                view[element].setProperty(
+                                    p.name, p.value, p.priority)
+                                specificities[element][p.name] = selector.specificity
+                            else:
+                                sameprio = (p.priority == view[element].getPropertyPriority(p.name))
+                                if not sameprio and bool(p.priority) or (sameprio and selector.specificity >= specificities[element][p.name]):
+                                    # later, more specific or higher prio
+                                    view[element].setProperty(p.name, p.value, p.priority)
+
+                except ExpressionError:
+                    if str(sys.exc_info()[1]) not in self.CSSErrors:
+                        self.CSSErrors.append(str(sys.exc_info()[1]))
+                    pass
+        return view
