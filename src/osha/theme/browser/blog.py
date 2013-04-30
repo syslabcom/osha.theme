213c2b7facb06a62a59f4e1867c076ad1eaae6cc
jure <jure@97d050b9-b799-4550-a901-4f04598d4601>
2013-04-30 14:32:28 +0000

    Update blog view to display blog intro text and description. Also don't display RSS link if there are no blog items.

--- src/osha/theme/browser/blog.py
+++ src/osha/theme/browser/blog.py
@@ -1,6 +1,8 @@
 from Products.Five.browser import BrowserView
 from plone import api
 
+BLOG_PATH = '/en/blog'
+
 
 class BlogView(BrowserView):
     """View that displays a list of blog entries."""
@@ -8,19 +10,30 @@ class BlogView(BrowserView):
     def __call__(self):
         return self.index()
 
-    def get_blog_items(self):
+    def blog_description(self):
+        """Return the blog description"""
+        portal = api.portal.get()
+        try:
+            blog_path = "/".join(portal.getPhysicalPath()) + BLOG_PATH
+            blog = portal.restrictedTraverse(blog_path)
+            return blog.getDescription()
+        except (AttributeError, KeyError):
+            # blog not found
+            return
+
+    def blog_items(self):
         """Return a list of blog items. If blog entry is not available in
         currently selected language, use the canonical version ('en').
 
         :returns: a list of 'Blog Entry' objects
         """
+        portal = api.portal.get()
+        path_en = "/".join(portal.getPhysicalPath()) + BLOG_PATH
         catalog = api.portal.get_tool('portal_catalog')
-        blog_en = self.context.getCanonical()
-        path = '/'.join(blog_en.getPhysicalPath())
         items_en = catalog(
             portal_type=['Blog Entry'],
             Language='all',
-            path={"query": path, "depth": 2},
+            path={"query": path_en},
             sort_on='effective',
             sort_order='descending'
         )
@@ -36,3 +49,18 @@ class BlogView(BrowserView):
                 results.append(obj)
 
         return results
+
+    def blog_intro(self):
+        """Return introductory text which we fetch from a page in the folder
+        with id 'blog-intro'. If page is not available in currently selected
+        language, use the canonical version ('en').
+        """
+        page = self.context.get('blog-intro') or \
+               self.context.getCanonical().get('blog-intro')
+
+        try:
+            text = page and page.getText() or None
+        except AttributeError:
+            # object found but doesn't have a text field (wrong type)
+            text = None
+        return text
