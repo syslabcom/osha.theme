9fbf89734d99b3b8e5f54123d6fa1881a98799a4
jure <jure@97d050b9-b799-4550-a901-4f04598d4601>
2013-05-17 01:07:59 +0000

    register blog view for the Document type, read intro text and blog description from the context, refs #6725

--- src/osha/theme/browser/blog.py
+++ src/osha/theme/browser/blog.py
@@ -1,7 +1,7 @@
 from Products.Five.browser import BrowserView
 from plone import api
 
-BLOG_PATH = '/en/blog'
+BLOG_PATH = '/en/about/director_corner'
 
 
 class BlogView(BrowserView):
@@ -9,17 +9,6 @@ class BlogView(BrowserView):
 
     def __call__(self):
         return self.index()
-
-    def blog_description(self):
-        """Return the blog description"""
-        portal = api.portal.get()
-        try:
-            blog_path = "/".join(portal.getPhysicalPath()) + BLOG_PATH
-            blog = portal.restrictedTraverse(blog_path)
-            return blog.getDescription()
-        except (AttributeError, KeyError):
-            # blog not found
-            return
 
     def blog_items(self):
         """Return a list of blog items. If blog entry is not available in
@@ -49,18 +38,3 @@ class BlogView(BrowserView):
                 results.append(obj)
 
         return results
-
-    def blog_intro(self):
-        """Return introductory text which we fetch from a page in the folder
-        with id 'blog-intro'. If page is not available in currently selected
-        language, use the canonical version ('en').
-        """
-        page = self.context.get('blog-intro') or \
-               self.context.getCanonical().get('blog-intro')
-
-        try:
-            text = page and page.getText() or None
-        except AttributeError:
-            # object found but doesn't have a text field (wrong type)
-            text = None
-        return text
