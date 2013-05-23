a06aae0a45714a56f170b3c498e6130c7575cbe9
jure <jure@97d050b9-b799-4550-a901-4f04598d4601>
2013-05-23 21:08:39 +0000

    updated blog views as per latest change requests (create director's corner view, update rss view etc.), refs #6725

--- src/osha/theme/browser/blog.py
+++ src/osha/theme/browser/blog.py
@@ -1,24 +1,28 @@
 from Products.Five.browser import BrowserView
 from plone import api
 
-BLOG_PATH = '/en/about/director_corner'
-
 
 class BlogView(BrowserView):
-    """View that displays a list of blog entries."""
+    """View for the front page of the Blog section."""
 
     def __call__(self):
         return self.index()
 
     def blog_items(self):
+        """Return blog items from the blog folder"""
+        return self._get_blog_items(folder=self.context.aq_parent)
+
+    def _get_blog_items(self, folder=None):
         """Return a list of blog items. If blog entry is not available in
         currently selected language, use the canonical version ('en').
 
+        :param folder: folder where to get the blog items from
         :returns: a list of 'Blog Entry' objects
         """
-        portal = api.portal.get()
-        path_en = "/".join(portal.getPhysicalPath()) + BLOG_PATH
+        if not folder:
+            return
         catalog = api.portal.get_tool('portal_catalog')
+        path_en = '/'.join(folder.getCanonical().getPhysicalPath())
         items_en = catalog(
             portal_type=['Blog Entry'],
             Language='all',
@@ -38,3 +42,35 @@ class BlogView(BrowserView):
                 results.append(obj)
 
         return results
+
+
+class DirectorCornerView(BlogView):
+    """View for the front page of Director's Corner."""
+
+    def __call__(self):
+        return self.index()
+
+    def blog_items(self):
+        """Return blog items from the blog subfolder"""
+        blog_folder = self.context.aq_parent.get('blog')
+        if blog_folder:
+            return self._get_blog_items(folder=blog_folder)
+
+    def blog_intro(self):
+        """Return the blog introductory text."""
+        try:
+            blog_page = self.context.aq_parent['blog']['front-page']
+            return blog_page.getText()
+        except (AttributeError, KeyError):
+            return
+
+
+class BlogRssView(BlogView):
+    """RSS view for the blog folder"""
+
+    def __call__(self):
+        return self.index()
+
+    def blog_items(self):
+        """Return blog items from this folder."""
+        return self._get_blog_items(folder=self.context)
