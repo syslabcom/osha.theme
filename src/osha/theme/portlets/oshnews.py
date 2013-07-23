f7ed139b8ebb335363b9d8e42f6597b45431b6d7
Wolfgang Thomas <thomas@syslab.com>
2013-07-23 17:18:57 +0200

    flake8

--- src/osha/theme/portlets/oshnews.py
+++ src/osha/theme/portlets/oshnews.py
@@ -1,8 +1,6 @@
-from AccessControl.SecurityManagement import getSecurityManager
 from Acquisition import aq_parent, aq_inner
 from DateTime import DateTime
 from collective.solr.mangler import iso8601date
-from osha.theme.browser.utils import search_solr
 from plone.memoize import ram
 from plone.memoize.compress import xhtml_compress
 from plone.memoize.instance import memoize
@@ -18,71 +16,69 @@ from zope.component import getMultiAdapter
 from zope.formlib import form
 from zope.interface import implements
 
+
 class INewsPortlet(IPortletDataProvider):
 
     count = schema.Int(
-            title=_(u'Number of items to display'),
-            description=_(u'How many items to list.'),
-            required=True,
-            default=5
-            )
+        title=_(u'Number of items to display'),
+        description=_(u'How many items to list.'),
+        required=True,
+        default=5
+    )
     state = schema.Tuple(
-            title=_(u"Workflow state"),
-            description=_(u"Items in which workflow state to show."),
-            default=('published', ),
-            required=True,
-            value_type=schema.Choice(
-                vocabulary="plone.app.vocabularies.WorkflowStates")
-            )
+        title=_(u"Workflow state"),
+        description=_(u"Items in which workflow state to show."),
+        default=('published', ),
+        required=True,
+        value_type=schema.Choice(
+            vocabulary="plone.app.vocabularies.WorkflowStates")
+    )
     subject = schema.Tuple(
-            title=_(u"Categories"),
-            description=_(
-                    u"Pick one or more categories for which you want to show "
-                    "events."),
-            default=tuple(),
-            required=False,
-            value_type=schema.Choice(
-                vocabulary="osha.policy.vocabularies.categories")
-            )
+        title=_(u"Categories"),
+        description=_(
+            u"Pick one or more categories for which you want to show "
+            "events."),
+        default=tuple(),
+        required=False,
+        value_type=schema.Choice(
+            vocabulary="osha.policy.vocabularies.categories")
+    )
     newsfolder_path = schema.TextLine(
-            title=_(u'Newsfolder path'),
-            description=(
-                    u"Enter a folder where the 'more news' link will "
-                    "point to. This is optional. If you add a %s, "
-                    "it will be replaced by the current language."
-                    ),
-            required=False,
-            )
+        title=_(u'Newsfolder path'),
+        description=(
+            u"Enter a folder where the 'more news' link will "
+            "point to. This is optional. If you add a %s, "
+            "it will be replaced by the current language."
+        ),
+        required=False,
+    )
     rss_path = schema.TextLine(
-            title=_(u'RSS path'),
-            description=_(
-                    u'Enter a relative path to the folder or topic that '
-                    'displays an RSS representation of the news. '
-                    '"/RSS" will automatically be appended to the URL.'
-                    'This setting is optional'
-                    ),
-            required=False,
-            )
+        title=_(u'RSS path'),
+        description=_(
+            u'Enter a relative path to the folder or topic that '
+            'displays an RSS representation of the news. '
+            '"/RSS" will automatically be appended to the URL.'
+            'This setting is optional'
+        ),
+        required=False,
+    )
     rss_explanation_path = schema.TextLine(
-            title=_(u'RSS explanation path'),
-            description=_(
-                    u'Enter a relative path to a page that gives '
-                    'general RSS information. This is optional.'
-                    ),
-            required=False,
-            )
+        title=_(u'RSS explanation path'),
+        description=_(
+            u'Enter a relative path to a page that gives '
+            'general RSS information. This is optional.'
+        ),
+        required=False,
+    )
 
 
 class Assignment(base.Assignment):
     implements(INewsPortlet)
 
-    def __init__(self,
-                count=5,
-                state=('published', ),
-                subject=tuple(),
-                newsfolder_path='',
-                rss_path='',
-                rss_explanation_path=''):
+    def __init__(
+        self, count=5, state=('published', ), subject=tuple(),
+        newsfolder_path='', rss_path='', rss_explanation_path=''
+    ):
 
         self.count = count
         self.state = state
@@ -125,7 +121,6 @@ class Renderer(base.Renderer):
         if not hasattr(self.data, 'subject'):
             self.data.subject = tuple()
 
-
     def published_news_items(self):
         return self._data()
 
@@ -136,7 +131,6 @@ class Renderer(base.Renderer):
         subject = self.data.subject
         navigation_root_path = self.navigation_root_path
         return (newsfolder_path, preflang, subject, navigation_root_path)
-
 
     @ram.cache(_render_cachekey)
     @property
@@ -164,8 +158,8 @@ class Renderer(base.Renderer):
 
         # make sure to exclude the subs
         query = '(portal_type:"News Item" OR isNews:true) AND ' \
-        'review_state:(%(review_state)s) AND path_parents:(%(path)s) ' \
-        'AND effective:[* TO %(effective)s]' % \
+            'review_state:(%(review_state)s) AND path_parents:(%(path)s) ' \
+            'AND effective:[* TO %(effective)s]' % \
             {'review_state': ' OR '.join(self.data.state),
              'path': path,
              'effective': iso8601date(DateTime()), }
@@ -173,9 +167,10 @@ class Renderer(base.Renderer):
         if subject:
             query += ' AND Subject:(%s)' % ' OR '.join(subject)
 
-        lf_search_view = self.context.restrictedTraverse("@@language-fallback-search")
+        lf_search_view = self.context.restrictedTraverse(
+            "@@language-fallback-search")
         results = lf_search_view.search_solr(
-            query, sort='Date desc', rows=limit) #, lang_query=False)
+            query, sort='Date desc', rows=limit)  # lang_query=False)
         items = list()
         for res in results[:limit]:
             try:
